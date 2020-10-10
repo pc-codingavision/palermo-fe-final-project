9c650fff69e99e25d3573da75f7e7a72792cc7a9
Piero Cascio <piero.cascio@gmail.com>
2020-10-10 10:34:02 +0200

    Create skeleton Angular app.

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cav-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'cav-final-project';
+}
