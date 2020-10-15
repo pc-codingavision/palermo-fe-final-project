cf04b3e3e958a06453450cf09ea08328b2081fcf
zeceajnv <ludovico.zitelli@gmail.com>
2020-10-15 18:48:25 +0200

    Added tenant module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { TenantModule } from './modules/core/tenant/tenant.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, TenantModule],
   providers: [],
   bootstrap: [AppComponent],
 })
