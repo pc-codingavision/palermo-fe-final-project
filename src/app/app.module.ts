8038ce75d81b560cc8b069f68844f99098270239
zeceajnv <ludovico.zitelli@gmail.com>
2020-10-15 18:45:26 +0200

    Added Landlord module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { LandlordModule } from './modules/core/landlord/landlord.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, LandlordModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/modules/core/landlord/landlord.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LandlordRoutingModule } from './landlord-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LandlordRoutingModule
+  ]
+})
+export class LandlordModule { }
