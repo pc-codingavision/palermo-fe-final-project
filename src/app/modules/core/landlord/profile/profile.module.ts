09fa3327553d4eea4884449c1a990d8eec36bfc1
Fanni Felfoldi <felfoldi26fanni@gmail.com>
2020-10-16 11:09:51 +0200

    add edit.component

--- src/app/modules/core/landlord/profile/profile.module.ts
+++ src/app/modules/core/landlord/profile/profile.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 
 import { ProfileRoutingModule } from './profile-routing.module'
-import { ViewComponent } from './view/view.component'
+import { ViewComponent } from './view/view.component';
+import { EditComponent } from './edit/edit.component'
 
 @NgModule({
-  declarations: [ViewComponent],
+  declarations: [ViewComponent, EditComponent],
   imports: [CommonModule, ProfileRoutingModule],
 })
 export class ProfileModule {}
