76c0c7830764bdb58305e81b6a41cae512dc08d0
zeceajnv <ludovico.zitelli@gmail.com>
2020-10-16 11:48:39 +0200

    Added lazy loading feature for tenant module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path: 'tenant',
+    loadChildren: () =>
+      import('./modules/core/tenant/tenant.module').then((l) => l.TenantModule),
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
