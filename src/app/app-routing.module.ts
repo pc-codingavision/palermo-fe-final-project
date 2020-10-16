12632044de8b3b89a99d99b272e8b39ce1cd21cd
Fanni Felfoldi <felfoldi26fanni@gmail.com>
2020-10-16 10:11:25 +0200

    add path for landlord lazyloading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path: 'lanlord',
+    loadChildren: () =>
+      import('./modules/core/landlord/landlord.module').then((l) => l.LandlordModule),
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
