d19b76f5aa3120641474b40cb512b608161f8d35
zeceajnv <ludovico.zitelli@gmail.com>
2020-10-16 16:18:05 +0200

    Added tenant/profile module with routing

--- src/app/modules/core/tenant/tenant-routing.module.ts
+++ src/app/modules/core/tenant/tenant-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = []
+const profileModule = () =>
+  import('./profile/profile.module').then((p) => p.ProfileModule)
+
+const routes: Routes = [
+  {
+    path: 'profile',
+    loadChildren: profileModule,
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
