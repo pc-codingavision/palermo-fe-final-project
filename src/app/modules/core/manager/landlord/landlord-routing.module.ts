413bd589f10f8f27c7c28f68cd714601390a8b8a
Vincenzo La Mantia <vincenzoguglielmo@gmail.com>
2020-10-17 13:17:01 +0200

    Add view component

--- src/app/modules/core/manager/landlord/landlord-routing.module.ts
+++ src/app/modules/core/manager/landlord/landlord-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { EditComponent } from './edit/edit.component'
+import { ViewComponent } from './view/view.component'
 import { ListComponent } from './list/list.component'
 import { NewComponent } from './new/new.component'
 
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: 'list', component: ListComponent },
   { path: 'new', component: NewComponent },
   { path: 'edit', component: EditComponent },
+  { path: 'view', component: ViewComponent },
 ]
 
 @NgModule({
--- src/app/modules/core/manager/landlord/landlord.module.ts
+++ src/app/modules/core/manager/landlord/landlord.module.ts
@@ -5,10 +5,11 @@ import { LandlordRoutingModule } from './landlord-routing.module';
 import { ListComponent } from './list/list.component';
 import { NewComponent } from './new/new.component';
 import { EditComponent } from './edit/edit.component';
+import { ViewComponent } from './view/view.component';
 
 
 @NgModule({
-  declarations: [ListComponent, NewComponent, EditComponent],
+  declarations: [ListComponent, NewComponent, EditComponent, ViewComponent],
   imports: [
     CommonModule,
     LandlordRoutingModule
