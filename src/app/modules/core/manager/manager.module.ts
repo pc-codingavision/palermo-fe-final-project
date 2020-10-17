7a0880eb4f7c59a325a1c53d4c6c9da39bd4d227
Carmelo <melonemark8@gmail.com>
2020-10-17 11:59:21 +0200

    create lazy loading tenant module

--- src/app/modules/core/manager/manager.module.ts
+++ src/app/modules/core/manager/manager.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 
 import { ManagerComponent } from './container/manager-container.component'
-import { ManagerRoutingModule } from './manager-routing.module'
+import { ManagerRoutingModule } from './manager-routing.module';
+import { TenantModule } from './tenant/tenant.module'
 
 @NgModule({
   declarations: [ManagerComponent],
-  imports: [CommonModule, ManagerRoutingModule],
+  imports: [CommonModule, ManagerRoutingModule, TenantModule],
 })
 export class ManagerModule {}
---
+++ src/app/modules/core/manager/tenant/tenant.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TenantRoutingModule } from './tenant-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TenantRoutingModule
+  ]
+})
+export class TenantModule { }
