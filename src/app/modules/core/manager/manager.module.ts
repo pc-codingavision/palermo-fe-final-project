50ac2bd0944e53c0ca618fb7706b1179802cf887
Gabriele Bilello <gabriele.bilello1@gmail.com>
2020-10-17 11:47:11 +0200

    Add reservation module with lazy loading

--- src/app/modules/core/manager/manager.module.ts
+++ src/app/modules/core/manager/manager.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 
 import { ManagerComponent } from './container/manager-container.component'
-import { ManagerRoutingModule } from './manager-routing.module'
+import { ManagerRoutingModule } from './manager-routing.module';
+import { ReservationModule } from './reservation/reservation.module'
 
 @NgModule({
   declarations: [ManagerComponent],
-  imports: [CommonModule, ManagerRoutingModule],
+  imports: [CommonModule, ManagerRoutingModule, ReservationModule],
 })
 export class ManagerModule {}
---
+++ src/app/modules/core/manager/reservation/reservation.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReservationRoutingModule } from './reservation-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ReservationRoutingModule
+  ]
+})
+export class ReservationModule { }
