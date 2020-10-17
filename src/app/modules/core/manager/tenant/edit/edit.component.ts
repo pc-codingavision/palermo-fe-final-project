0a6c7aa1a45364ee11ad819b6ecb3395430ea1cb
Cosimo Nigrelli <nigrellicosimo@gmail.com>
2020-10-17 12:22:59 +0200

    create edit component

---
+++ src/app/modules/core/manager/tenant/edit/edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cav-edit',
+  templateUrl: './edit.component.html',
+  styleUrls: ['./edit.component.scss']
+})
+export class EditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
