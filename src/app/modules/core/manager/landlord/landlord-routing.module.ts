555882793f79275cfd95d611eaf35412ef5cf031
Matteo Oliva <olivamatteus@gmail.com>
2020-10-17 13:03:41 +0200

    Add component list.

--- src/app/modules/core/manager/landlord/landlord-routing.module.ts
+++ src/app/modules/core/manager/landlord/landlord-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ListComponent } from './list/list.component';
+
+const routes: Routes = [
+  { path: 'list', component: ListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/core/manager/landlord/landlord.module.ts
+++ src/app/modules/core/manager/landlord/landlord.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LandlordRoutingModule } from './landlord-routing.module';
+import { ListComponent } from './list/list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ListComponent],
   imports: [
     CommonModule,
     LandlordRoutingModule
