5202696fc5a6c45ae4034f89820de1847f265b7f
Matteo Oliva <olivamatteus@gmail.com>
2020-10-17 13:06:09 +0200

    Add component new.

--- src/app/modules/core/manager/landlord/landlord-routing.module.ts
+++ src/app/modules/core/manager/landlord/landlord-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ListComponent } from './list/list.component';
+import { NewComponent } from './new/new.component';
 
 const routes: Routes = [
-  { path: 'list', component: ListComponent }
+  { path: 'list', component: ListComponent },
+  { path: 'new', component: NewComponent }
 ];
 
 @NgModule({
--- src/app/modules/core/manager/landlord/landlord.module.ts
+++ src/app/modules/core/manager/landlord/landlord.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { LandlordRoutingModule } from './landlord-routing.module';
 import { ListComponent } from './list/list.component';
+import { NewComponent } from './new/new.component';
 
 
 @NgModule({
-  declarations: [ListComponent],
+  declarations: [ListComponent, NewComponent],
   imports: [
     CommonModule,
     LandlordRoutingModule
