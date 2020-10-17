aeafad4e62f6d7e2f9e183c509ea653c679ea6a9
Carmelo <melonemark8@gmail.com>
2020-10-17 12:03:46 +0200

    create route path for list component

--- src/app/modules/core/manager/tenant/tenant-routing.module.ts
+++ src/app/modules/core/manager/tenant/tenant-routing.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component'
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'list', component: ListComponent }]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class TenantRoutingModule { }
+export class TenantRoutingModule {}
