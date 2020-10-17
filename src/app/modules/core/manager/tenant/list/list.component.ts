8975aae5549403b6e6c4896c415a2ae8af839dda
Carmelo <melonemark8@gmail.com>
2020-10-17 12:03:13 +0200

    create list component

---
+++ src/app/modules/core/manager/tenant/list/list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cav-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
