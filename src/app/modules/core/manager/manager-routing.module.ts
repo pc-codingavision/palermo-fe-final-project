33bec1a59dd3daf2b77ae271afcc352d399f4864
Matteo Oliva <olivamatteus@gmail.com>
2020-10-17 12:59:26 +0200

    Configured manager routing with lazy loading.

--- src/app/modules/core/manager/manager-routing.module.ts
+++ src/app/modules/core/manager/manager-routing.module.ts
@@ -3,10 +3,14 @@ import { RouterModule, Routes } from '@angular/router'
 
 import { ManagerComponent } from './container/manager-container.component'
 
-const routes: Routes = [{ path: '', component: ManagerComponent }]
+const landlordModule = () => import('./landlord/landlord.module').then(l => l.LandlordModule);
+const routes: Routes = [
+  { path: '', component: ManagerComponent },
+  { path: 'landlord', loadChildren: landlordModule }
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class ManagerRoutingModule {}
+export class ManagerRoutingModule { }
