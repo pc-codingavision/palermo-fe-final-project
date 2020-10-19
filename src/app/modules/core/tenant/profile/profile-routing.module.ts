910d1c538841336e401237a1859c5200ac64db87
zeceajnv <ludovico.zitelli@gmail.com>
2020-10-19 18:29:34 +0200

    Fixed profile tenant/routing with edit and view components

--- src/app/modules/core/tenant/profile/profile-routing.module.ts
+++ src/app/modules/core/tenant/profile/profile-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = []
+import { ProfileEditComponent } from './components/profile-edit/profile-edit.component'
+import { ProfileViewComponent } from './components/profile-view/profile-view.component'
+
+const routes: Routes = [
+  { path: 'view', component: ProfileViewComponent },
+  { path: 'edit', component: ProfileEditComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
