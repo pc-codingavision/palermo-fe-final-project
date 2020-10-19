8d502f50546e6ec7482a7f4bbf562dae255c1cb6
Giulia Reas <56250206+lareas123@users.noreply.github.com>
2020-10-19 17:26:42 +0200

    fixed folder structure

--- src/app/modules/core/landlord/landlord-profile/landlord-profile-routing.module.ts
+++ src/app/modules/core/landlord/landlord-profile/landlord-profile-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
-import { LandlordProfileEditComponent } from './landlord-profile-container/landlord-profile-edit/landlord-profile-edit.component'
-import { LandlordProfileViewComponent } from './landlord-profile-container/landlord-profile-view/landlord-profile-view.component'
+import { LandlordProfileEditComponent } from './components/landlord-profile-edit/landlord-profile-edit.component'
+import { LandlordProfileViewComponent } from './components/landlord-profile-view/landlord-profile-view.component'
 
 const routes: Routes = [
   { path: 'view', component: LandlordProfileViewComponent },
