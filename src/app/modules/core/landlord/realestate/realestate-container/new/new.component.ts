48b5444e1398213d7b0c0b6e2a58081c9b20e7ac
Paolo Tabacco <tabaccopaolo99@gmail.com>
2020-10-19 17:46:57 +0200

    Fixed error new component

---
+++ src/app/modules/core/landlord/realestate/realestate-container/new/new.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'cav-add-new',
+  templateUrl: './new.component.html',
+  styleUrls: ['./new.component.scss'],
+})
+export class NewComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
