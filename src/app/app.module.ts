f4dba56dc85752435298a50e4ad74178a9976e2c
Piero Cascio <piero.cascio@gmail.com>
2020-10-20 11:29:07 +0200

    Add Angular Material library dependency.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { MaterialModule } from './modules/shared/material.module'
+import { MaterialModule } from './modules/shared/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FlexLayoutModule, MaterialModule],
+  imports: [BrowserModule, AppRoutingModule, FlexLayoutModule, MaterialModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
