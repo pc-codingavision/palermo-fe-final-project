98fb10eb200051c53d49914a07325724d957727b
Gabriele Bilello <gabriele.bilello1@gmail.com>
2020-10-20 17:51:28 +0200

    Created shared models

---
+++ src/app/shared/models/users.ts
@@ -0,0 +1,34 @@
+import { Role } from '../enum/role'
+
+export abstract class Name {
+  firstName: string
+  middleName?: string
+  surname: string
+
+  getFullName(): string {
+    return `${this.firstName} ${this.surname}`
+  }
+}
+
+export interface IUser {
+  _id: number
+  name: Name
+  mail: string
+  phone: string
+  role: Role
+  username: string
+  password: string
+  picture: string
+}
+
+export abstract class Address {
+  line1: string
+  line2?: string
+  city: string
+  state: string
+  postCode: string
+
+  getFullAddress(): string {
+    return `${this.line1}, ${this.city}, ${this.state}, ${this.postCode}`
+  }
+}
