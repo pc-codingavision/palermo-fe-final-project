1305bc562fa6ddeb72850ffaea8927d87b64fb86
Paolo Tabacco <tabaccopaolo99@gmail.com>
2020-10-20 12:22:28 +0200

    Updated names and interfaces.

--- src/app/shared/models/reservation.ts
+++ src/app/shared/models/reservation.ts
@@ -1,11 +1,16 @@
-export interface Reservation {
-  id: number
-  tenant_name: string
-  tenant_surname: string
-  tenant_email: string
-  tenant_phone: number
-  guest_number: number
-  check_in: Date
-  check_out: Date
-  special_request: string
+export interface ITenantName {
+  name: string
+  middle?: string
+  surname: string
+}
+
+export interface IReservation {
+  reservationId: number
+  tenantName: ITenantName
+  tenantEmail: string
+  tenantPhone: number
+  guestNumber: number
+  checkIn: Date
+  checkOut: Date
+  specialRequest?: string
 }
