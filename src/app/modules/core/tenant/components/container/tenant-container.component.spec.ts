968e9dc1ac16df6dd3f024a1aecb557b263f0875
Piero Cascio <piero.cascio@gmail.com>
2020-10-20 18:10:18 +0200

    Execute npm style:fix and lint:fix goals.

--- src/app/modules/core/tenant/components/container/tenant-container.component.spec.ts
+++ src/app/modules/core/tenant/components/container/tenant-container.component.spec.ts
@@ -1,25 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { TenantContainerComponent } from './tenant-container.component';
+import { TenantContainerComponent } from './tenant-container.component'
 
 describe('TenantContainerComponent', () => {
-  let component: TenantContainerComponent;
-  let fixture: ComponentFixture<TenantContainerComponent>;
+  let component: TenantContainerComponent
+  let fixture: ComponentFixture<TenantContainerComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TenantContainerComponent ]
-    })
-    .compileComponents();
-  });
+      declarations: [TenantContainerComponent],
+    }).compileComponents()
+  })
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TenantContainerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(TenantContainerComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
--- src/app/modules/core/tenant/components/container/tenant-container.component.ts
+++ src/app/modules/core/tenant/components/container/tenant-container.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'cav-tenant-container',
   templateUrl: './tenant-container.component.html',
-  styleUrls: ['./tenant-container.component.scss']
+  styleUrls: ['./tenant-container.component.scss'],
 })
 export class TenantContainerComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
