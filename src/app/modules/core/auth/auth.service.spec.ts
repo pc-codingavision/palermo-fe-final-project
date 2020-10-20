e02e2308339585c1dfe9b0bf7dce49f1d29d150c
Piero Cascio <piero.cascio@gmail.com>
2020-10-20 08:25:39 +0200

    Fix broken test.

--- src/app/modules/core/auth/auth.service.spec.ts
+++ src/app/modules/core/auth/auth.service.spec.ts
@@ -6,7 +6,9 @@ describe('AuthService', () => {
   let service: AuthService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({})
+    TestBed.configureTestingModule({
+      providers: [{ provide: AuthService, useClass: AuthService }],
+    })
     service = TestBed.inject(AuthService)
   })
 
