614f8877131cd539e81205ded36c126813546045
Piero Cascio <piero.cascio@gmail.com>
2020-10-20 18:06:00 +0200

    Add tenant container.

---
+++ src/app/modules/core/tenant/components/container/tenant-container.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TenantContainerComponent } from './tenant-container.component';
+
+describe('TenantContainerComponent', () => {
+  let component: TenantContainerComponent;
+  let fixture: ComponentFixture<TenantContainerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TenantContainerComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TenantContainerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/core/tenant/components/container/tenant-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cav-tenant-container',
+  templateUrl: './tenant-container.component.html',
+  styleUrls: ['./tenant-container.component.scss']
+})
+export class TenantContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
