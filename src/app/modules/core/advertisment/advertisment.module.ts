d8e590c3dc39a2445cb96f0b9d32c2d7b0984e9f
Fanni Felfoldi <felfoldi26fanni@gmail.com>
2020-10-21 12:06:07 +0200

    creat advertisment module

---
+++ src/app/modules/core/advertisment/advertisment.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdvertismentRoutingModule } from './advertisment-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AdvertismentRoutingModule
+  ]
+})
+export class AdvertismentModule { }
