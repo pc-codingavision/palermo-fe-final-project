e7576ffc6d345cda61d888057e4cc1b4d6523e57
Carmelo <melonemark8@gmail.com>
2020-10-21 18:28:37 +0200

    fix tslint problem

--- src/app/shared/models/tenant.ts
+++ src/app/shared/models/tenant.ts
@@ -1,6 +1,7 @@
 import { Role } from '../enum/role'
 import { IAddress, IName, User } from './users'
 
+// tslint:disable-next-line: no-empty-interface
 export interface IReservation {}
 
 export class Tenant extends User {
