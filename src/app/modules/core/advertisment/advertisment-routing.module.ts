4f468f781abf76fd597d0ad6eda29ff3ec1dd87c
Fanni Felfoldi <felfoldi26fanni@gmail.com>
2020-10-21 12:30:05 +0200

    add routing for component

--- src/app/modules/core/advertisment/advertisment-routing.module.ts
+++ src/app/modules/core/advertisment/advertisment-routing.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = [];
+import { AdvertismentListContainerComponent } from './components/advertisment-list-container/advertisment-list-container.component'
+
+const routes: Routes = [{ path: 'list', component: AdvertismentListContainerComponent }]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AdvertismentRoutingModule { }
+export class AdvertismentRoutingModule {}
