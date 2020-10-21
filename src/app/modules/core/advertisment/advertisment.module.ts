6186a274b2f4d4e277b9e1b4ae676898ce415361
Fanni Felfoldi <felfoldi26fanni@gmail.com>
2020-10-21 12:20:58 +0200

    run style:fix and lint:fix

--- src/app/modules/core/advertisment/advertisment.module.ts
+++ src/app/modules/core/advertisment/advertisment.module.ts
@@ -1,14 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
 
-import { AdvertismentRoutingModule } from './advertisment-routing.module';
-
+import { AdvertismentRoutingModule } from './advertisment-routing.module'
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    AdvertismentRoutingModule
-  ]
+  imports: [CommonModule, AdvertismentRoutingModule],
 })
-export class AdvertismentModule { }
+export class AdvertismentModule {}
