1a8ec33996ec6534fb8cf3510e47098f7d6f66e9
Gabriele Bilello <gabriele.bilello1@gmail.com>
2020-10-23 17:38:11 +0200

    Refactor class manager

--- src/app/shared/models/manager.ts
+++ src/app/shared/models/manager.ts
@@ -1,31 +1,34 @@
 import { Role } from '../enum/role'
-import { IAddress, IName, User } from './users'
+import { IAddress, IName, IUser } from './users'
 
-export class Manager extends User {
-  constructor(
-    id: number,
-    name: IName,
-    phone: string,
-    mail: string,
-    picture: string,
-    username: string,
-    password: string,
-    userStatus: boolean,
-    dateOfBirth: Date | null | string,
-    address?: IAddress
-  ) {
-    super(
-      id,
-      name,
-      phone,
-      mail,
-      picture,
-      username,
-      password,
-      userStatus,
-      dateOfBirth,
-      address
-    )
-    this.role = Role.Manager
+export class Manager implements IUser {
+  id: number
+  name: IName
+  phone: string
+  mail: string
+  picture: string
+  username: string
+  password: string
+  userStatus: boolean
+  dateOfBirth: string | Date
+  role: Role
+  address?: IAddress
+
+  constructor(user: Manager) {
+    this.id = user.id
+    this.name = user.name
+    this.phone = user.phone
+    this.mail = user.mail
+    this.picture = user.picture
+    this.username = user.username
+    this.password = user.password
+    this.userStatus = user.userStatus
+    this.dateOfBirth = user.dateOfBirth
+    this.role = user.role
+    this.address = user.address
+  }
+
+  static Build(manager: Manager): Manager {
+    return new this(manager)
   }
 }
