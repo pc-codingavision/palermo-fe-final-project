ddccebe2e4577ef1c86d4f48935ad8f1eb354bc3
Cosimo Nigrelli <nigrellicosimo@gmail.com>
2020-10-23 17:15:57 +0200

    Changed abstract class User to interface IUser

--- src/app/shared/models/users.ts
+++ src/app/shared/models/users.ts
@@ -12,40 +12,16 @@ export interface IAddress {
   state: string
   postCode: string
 }
-export abstract class User {
-  constructor(
-    public id: number,
-    public name: IName,
-    public phone: string,
-    public mail: string,
-    public role = Role,
-    public picture: string,
-    public username: string,
-    public password: string,
-    public userStatus: boolean,
-    public dateOfBirth: Date | null | string,
-    public address?: IAddress
-  ) {}
-  getFullName(): string {
-    if (!this.name) {
-      return ''
-    }
-
-    if (this.name.middleName) {
-      return `${this.name.firstName} ${this.name.middleName} ${this.name.surname}`
-    }
-
-    return `${this.name.firstName} ${this.name.surname}`
-  }
-  getFullAddress(): string {
-    if (!this.address) {
-      return ''
-    }
-
-    if (this.address.line2) {
-      return `${this.address.line1}, ${this.address.line2},  ${this.address.city}, ${this.address.state}, ${this.address.postCode}, `
-    }
-
-    return `${this.address.line1}, ${this.address.city}, ${this.address.state}, ${this.address.postCode}`
-  }
+export interface IUser {
+  id: number
+  name: IName
+  phone: string
+  mail: string
+  picture: string
+  username: string
+  password: string
+  userStatus: boolean
+  dateOfBirth: Date | null | string
+  role: Role
+  address?: IAddress
 }
