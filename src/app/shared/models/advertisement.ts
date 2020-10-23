9318c5b4830c88e7d3e895a7e5c54fa844a1f20c
Stefano Rizzo <stefanorizzo295@gmail.com>
2020-10-23 19:42:58 +0200

    Refactoring class Advertisement

--- src/app/shared/models/advertisement.ts
+++ src/app/shared/models/advertisement.ts
@@ -1,24 +1,45 @@
-import { Property } from './property'
+interface IReviews {
+  title: string
+  name: string
+  description: string
+}
 
-export class Advertisement extends Property {
-  getId() {
-    return `${this.id}`
+export class Advertisement {
+  landlordId: number
+  propertyId: number
+  imagePath: string[]
+  rooms: number
+  beds: number
+  numberOfToilet: number
+  city: string
+  description: string
+  facilities: {
+    tv: boolean
+    wifi: boolean
+    breakfastIncluded: boolean
+    parking: boolean
+    kitchen: boolean
+    shower: boolean
+    bath: boolean
+  }
+  reviews: IReviews[]
+  vote: number
+
+  private constructor(advs: Advertisement) {
+    this.landlordId = advs.landlordId
+    this.propertyId = advs.propertyId
+    this.imagePath = advs.imagePath
+    this.rooms = advs.rooms
+    this.beds = advs.beds
+    this.numberOfToilet = advs.numberOfToilet
+    this.city = advs.city
+    this.description = advs.description
+    this.facilities = advs.facilities
+    this.reviews = advs.reviews
+    this.vote = advs.vote
   }
 
-  getShortDescription() {
-    return `${this.rooms} - ${this.numberOfToilet} - ${this.beds} - ${this.toilet}
-    ${this.facilities}`
-  }
-
-  getLandlordProperty() {
-    return `${this.landlordName} - ${this.propertyName} - ${this.category}`
-  }
-
-  getImage() {
-    return `${this.imagePath}`
-  }
-
-  getReviews() {
-    return `${this.vote}`
+  static Build(advertisement: Advertisement): Advertisement {
+    return new this(advertisement)
   }
 }
