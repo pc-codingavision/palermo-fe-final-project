9b5ddd352c3c325708bbd9f213ff40d2320876c2
Ludovico Zitelli <ludovico.zitelli@gmail.com>
2020-10-24 13:13:06 +0200

    Added Reservation class

--- src/app/shared/models/reservation.ts
+++ src/app/shared/models/reservation.ts
@@ -7,3 +7,26 @@ export interface IReservation {
   checkOut: Date | string
   specialRequest?: string
 }
+
+export class Reservation implements IReservation {
+  reservationId: number
+  tenantId: number
+  propertyId: number
+  guestNumber: number
+  checkIn: Date | string
+  checkOut: Date | string
+  specialRequest?: string
+
+  private constructor(res: Reservation) {
+    this.reservationId = res.reservationId
+    this.tenantId = res.tenantId
+    this.propertyId = res.propertyId
+    this.checkIn = res.checkIn
+    this.checkOut = res.checkOut
+    this.specialRequest = res.specialRequest
+  }
+
+  static Build(reservation: Reservation): Reservation {
+    return new this(reservation)
+  }
+}
