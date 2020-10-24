65094b3d524cf38bca4531e09dd4d12435688de6
Stefano Rizzo <stefanorizzo295@gmail.com>
2020-10-24 20:08:36 +0200

    Fixed class Advertisement

--- src/app/shared/models/advertisement.ts
+++ src/app/shared/models/advertisement.ts
@@ -1,45 +1,24 @@
 interface IReviews {
   title: string
-  name: string
+  tenantId: number
   description: string
+  vote: number
 }
 
 export class Advertisement {
   landlordId: number
   propertyId: number
-  imagePath: string[]
-  rooms: number
-  beds: number
-  numberOfToilet: number
-  city: string
-  description: string
-  facilities: {
-    tv: boolean
-    wifi: boolean
-    breakfastIncluded: boolean
-    parking: boolean
-    kitchen: boolean
-    shower: boolean
-    bath: boolean
-  }
   reviews: IReviews[]
-  vote: number
 
   private constructor(advs: Advertisement) {
     this.landlordId = advs.landlordId
     this.propertyId = advs.propertyId
-    this.imagePath = advs.imagePath
-    this.rooms = advs.rooms
-    this.beds = advs.beds
-    this.numberOfToilet = advs.numberOfToilet
-    this.city = advs.city
-    this.description = advs.description
-    this.facilities = advs.facilities
     this.reviews = advs.reviews
-    this.vote = advs.vote
   }
 
   static Build(advertisement: Advertisement): Advertisement {
     return new this(advertisement)
   }
+
+  averageVote() {}
 }
