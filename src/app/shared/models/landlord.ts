7e0e326ef79b1cec53e2888343cc61076f667789
Cosimo Nigrelli <nigrellicosimo@gmail.com>
2020-10-25 10:59:24 +0100

    Added control for build method and role property

--- src/app/shared/models/landlord.ts
+++ src/app/shared/models/landlord.ts
@@ -1,5 +1,6 @@
 import { Role } from '../enum/role'
 import { IAddress, IName, IUser } from './users'
+
 export class Landlord implements IUser {
   private constructor(
     public id: number,
@@ -11,15 +12,21 @@ export class Landlord implements IUser {
     public password: string,
     public userStatus: boolean,
     public dateOfBirth: Date | null | string,
-    public role: Role.Landlord,
+    public role: Role,
     public address?: IAddress
   ) {}
 
   static Build(landlord: Landlord): Landlord {
+    if (!landlord) {
+      throw new Error('Insert a valid value')
+    }
     if (typeof landlord.dateOfBirth === 'string') {
       landlord.dateOfBirth = new Date(landlord.dateOfBirth)
     }
-    return new this(
+    if (landlord.role !== 'landlord') {
+      throw new Error('Role must be Landlord')
+    }
+    return new Landlord(
       landlord.id,
       landlord.name,
       landlord.phone,
