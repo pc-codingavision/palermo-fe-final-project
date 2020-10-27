abb34ba1cbf34628b11c0f632b42598f881ca296
Fanni Felfoldi <felfoldi26fanni@gmail.com>
2020-10-27 18:12:03 +0100

    add style to card-extra

--- src/app/modules/shared/material.module.ts
+++ src/app/modules/shared/material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core'
 import { MatCardModule } from '@angular/material/card'
+import { MatDividerModule } from '@angular/material/divider'
 import { MatGridListModule } from '@angular/material/grid-list'
 import { MatIconModule } from '@angular/material/icon'
 
-const modules = [MatCardModule, MatGridListModule, MatIconModule]
+const modules = [MatCardModule, MatGridListModule, MatIconModule, MatDividerModule]
 
 @NgModule({
   declarations: [],
