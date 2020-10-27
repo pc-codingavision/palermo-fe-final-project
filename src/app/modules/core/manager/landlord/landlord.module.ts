cec5c9417a8813df746451cf25d46a8a2670dc3d
Cosimo Nigrelli <nigrellicosimo@gmail.com>
2020-10-27 17:49:25 +0100

    Added Material Module

--- src/app/modules/core/manager/landlord/landlord.module.ts
+++ src/app/modules/core/manager/landlord/landlord.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 
+import { MaterialModule } from './../../../shared/material.module'
 import { LandlordContainerComponent } from './components/container/landlord-container.component'
 import { LandlordEditComponent } from './components/edit/landlord-edit.component'
 import { LandlordListComponent } from './components/list/landlord-list.component'
@@ -17,6 +18,6 @@ const landlordComponents = [
 ]
 @NgModule({
   declarations: [...landlordComponents],
-  imports: [CommonModule, LandlordRoutingModule],
+  imports: [CommonModule, LandlordRoutingModule, MaterialModule],
 })
 export class LandlordModule {}
