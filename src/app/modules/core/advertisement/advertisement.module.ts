b6911c4b8f0c4e15c1e1920d76a6631c40b3e6fc
Stefano Rizzo <stefanorizzo295@gmail.com>
2020-10-27 18:45:01 +0100

    Added modules in material-module

--- src/app/modules/core/advertisement/advertisement.module.ts
+++ src/app/modules/core/advertisement/advertisement.module.ts
@@ -1,20 +1,21 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 
+import { MaterialModule } from '../../shared/material.module'
 import { AdvertisementContainerComponent } from './../advertisement/components/advertisement-container/advertisement-container.component'
+import { DatesComponent } from '../advertisement/components/advertisement-list/properties-container/card-container/card-main-view/dates/dates.component'
+import { FacilityComponent } from '../advertisement/components/advertisement-list/properties-container/card-container/card-main-view/facilities-container/facility/facility.component'
+import { ScoreComponent } from '../advertisement/components/advertisement-list/properties-container/card-container/card-main-view/score/score.component'
+import { PropertiesContainerComponent } from '../advertisement/components/advertisement-list/properties-container/properties-container.component'
 import { AdvertisementRoutingModule } from './advertisement-routing.module'
 import { AdvertisementDetailsComponent } from './components/advertisement-details/advertisement-details.component'
 import { AdvertisementListComponent } from './components/advertisement-list/advertisement-list.component'
 import { CardContainerComponent } from './components/advertisement-list/properties-container/card-container/card-container.component'
 import { BookButtonComponent } from './components/advertisement-list/properties-container/card-container/card-main-view/book-button/book-button.component'
 import { CardMainViewComponent } from './components/advertisement-list/properties-container/card-container/card-main-view/card-main-view.component'
-import { DatesComponent } from './components/advertisement-list/properties-container/card-container/card-main-view/dates/dates.component'
 import { FacilitiesContainerComponent } from './components/advertisement-list/properties-container/card-container/card-main-view/facilities-container/facilities-container.component'
-import { FacilityComponent } from './components/advertisement-list/properties-container/card-container/card-main-view/facilities-container/facility/facility.component'
 import { FavouriteComponent } from './components/advertisement-list/properties-container/card-container/card-main-view/favourite/favourite.component'
 import { PhotogalleryComponent } from './components/advertisement-list/properties-container/card-container/card-main-view/photogallery/photogallery.component'
-import { ScoreComponent } from './components/advertisement-list/properties-container/card-container/card-main-view/score/score.component'
-import { PropertiesContainerComponent } from './components/advertisement-list/properties-container/properties-container.component'
 
 @NgModule({
   declarations: [
@@ -33,6 +34,6 @@ import { PropertiesContainerComponent } from './components/advertisement-list/pr
     BookButtonComponent,
   ],
 
-  imports: [CommonModule, AdvertisementRoutingModule],
+  imports: [CommonModule, AdvertisementRoutingModule, MaterialModule],
 })
 export class AdvertisementModule {}
--- src/app/modules/shared/material.module.ts
+++ src/app/modules/shared/material.module.ts
@@ -1,6 +1,19 @@
 import { NgModule } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { MatNativeDateModule } from '@angular/material/core'
+import { MatDatepickerModule } from '@angular/material/datepicker'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
 
-const modules = []
+const modules = [
+  MatButtonModule,
+  MatIconModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatFormFieldModule,
+  MatInputModule,
+]
 
 @NgModule({
   declarations: [],
