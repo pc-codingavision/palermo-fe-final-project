a0e4dc5efef785e0a00bd4aac887ff190fb44521
Cosimo Nigrelli <nigrellicosimo@gmail.com>
2020-10-27 17:50:00 +0100

    Created boilerplate for table list

--- src/app/modules/core/manager/landlord/components/list/landlord-list.component.ts
+++ src/app/modules/core/manager/landlord/components/list/landlord-list.component.ts
@@ -1,12 +1,32 @@
+import { animate, state, style, transition, trigger } from '@angular/animations'
 import { Component, OnInit } from '@angular/core'
+
+import { Landlord } from './../../../../../../shared/models/landlord'
+import { LANDLORDS } from './../../../../../../shared/models/mock-data/mock-landlord'
 
 @Component({
   selector: 'cav-landlord-list',
   templateUrl: './landlord-list.component.html',
   styleUrls: ['./landlord-list.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      ),
+    ]),
+  ],
 })
 export class LandlordListComponent implements OnInit {
-  constructor() {}
+  landlords: Landlord[] = []
+  dataSource = this.landlords
+  displayedColumns: string[] = ['id', 'fullName', 'mail', 'phone_number']
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    LANDLORDS.forEach((val) => {
+      this.landlords.push(Landlord.Build(val))
+    })
+  }
 }
