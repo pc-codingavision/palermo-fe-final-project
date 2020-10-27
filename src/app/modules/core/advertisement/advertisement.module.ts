ca8af1d8766d3f197afdf6e710856ba9d1c5f08b
Stefano Rizzo <stefanorizzo295@gmail.com>
2020-10-27 23:04:05 +0100

    Added  FlexLayout in advertisement.module

--- src/app/modules/core/advertisement/advertisement.module.ts
+++ src/app/modules/core/advertisement/advertisement.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { FlexLayoutModule } from '@angular/flex-layout'
 
 import { MaterialModule } from './../../shared/material.module'
 import { AdvertisementContainerComponent } from './../advertisement/components/advertisement-container/advertisement-container.component'
@@ -34,6 +35,6 @@ import { PhotogalleryComponent } from './components/advertisement-list/propertie
     BookButtonComponent,
   ],
 
-  imports: [CommonModule, AdvertisementRoutingModule, MaterialModule],
+  imports: [CommonModule, AdvertisementRoutingModule, MaterialModule, FlexLayoutModule],
 })
 export class AdvertisementModule {}
