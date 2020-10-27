0fe240e7f61725af4afaab90109080a69c034703
Paolo Tabacco <tabaccopaolo99@gmail.com>
2020-10-27 11:15:11 +0100

    Commit untracked files

--- src/app/modules/core/advertisement/advertisement.module.ts
+++ src/app/modules/core/advertisement/advertisement.module.ts
@@ -6,6 +6,7 @@ import { AdvertisementRoutingModule } from './advertisement-routing.module'
 import { AdvertisementDetailsComponent } from './components/advertisement-details/advertisement-details.component'
 import { AdvertisementListComponent } from './components/advertisement-list/advertisement-list.component'
 import { CardContainerComponent } from './components/advertisement-list/properties-container/card-container/card-container.component'
+import { BookButtonComponent } from './components/advertisement-list/properties-container/card-container/card-main-view/book-button/book-button.component'
 import { CardMainViewComponent } from './components/advertisement-list/properties-container/card-container/card-main-view/card-main-view.component'
 import { DatesComponent } from './components/advertisement-list/properties-container/card-container/card-main-view/dates/dates.component'
 import { FacilitiesContainerComponent } from './components/advertisement-list/properties-container/card-container/card-main-view/facilities-container/facilities-container.component'
@@ -13,8 +14,7 @@ import { FacilityComponent } from './components/advertisement-list/properties-co
 import { FavouriteComponent } from './components/advertisement-list/properties-container/card-container/card-main-view/favourite/favourite.component'
 import { PhotogalleryComponent } from './components/advertisement-list/properties-container/card-container/card-main-view/photogallery/photogallery.component'
 import { ScoreComponent } from './components/advertisement-list/properties-container/card-container/card-main-view/score/score.component'
-import { PropertiesContainerComponent } from './components/advertisement-list/properties-container/properties-container.component';
-import { BookButtonComponent } from './components/advertisement-list/properties-container/card-container/card-main-view/book-button/book-button.component'
+import { PropertiesContainerComponent } from './components/advertisement-list/properties-container/properties-container.component'
 
 @NgModule({
   declarations: [
