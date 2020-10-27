68a5405e9a5a0075359aaa0e019fc424b20450fc
Fanni Felfoldi <felfoldi26fanni@gmail.com>
2020-10-27 13:44:40 +0100

    add  materialModul to advertisementModule

--- src/app/modules/core/advertisement/advertisement.module.ts
+++ src/app/modules/core/advertisement/advertisement.module.ts
@@ -1,17 +1,18 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 
+import { MaterialModule } from './../../shared/material.module'
 import { AdvertisementContainerComponent } from './../advertisement/components/advertisement-container/advertisement-container.component'
 import { AdvertisementRoutingModule } from './advertisement-routing.module'
 import { AdvertisementDetailsComponent } from './components/advertisement-details/advertisement-details.component'
 import { AdvertisementListComponent } from './components/advertisement-list/advertisement-list.component'
 import { CardContainerComponent } from './components/advertisement-list/properties-container/card-container/card-container.component'
+import { AboutComponent } from './components/advertisement-list/properties-container/card-container/card-extra/about/about.component'
 import { CardExtraComponent } from './components/advertisement-list/properties-container/card-container/card-extra/card-extra.component'
-import { ReviewsContainerComponent } from './components/advertisement-list/properties-container/card-container/card-extra/reviews-container/reviews-container.component'
-import { PropertiesContainerComponent } from './components/advertisement-list/properties-container/properties-container.component';
-import { ReviewComponent } from './components/advertisement-list/properties-container/card-container/card-extra/reviews-container/review/review.component';
-import { AboutComponent } from './components/advertisement-list/properties-container/card-container/card-extra/about/about.component';
 import { FacilitiesComponent } from './components/advertisement-list/properties-container/card-container/card-extra/facilities/facilities.component'
+import { ReviewComponent } from './components/advertisement-list/properties-container/card-container/card-extra/reviews-container/review/review.component'
+import { ReviewsContainerComponent } from './components/advertisement-list/properties-container/card-container/card-extra/reviews-container/reviews-container.component'
+import { PropertiesContainerComponent } from './components/advertisement-list/properties-container/properties-container.component'
 
 @NgModule({
   declarations: [
@@ -27,6 +28,6 @@ import { FacilitiesComponent } from './components/advertisement-list/properties-
     FacilitiesComponent,
   ],
 
-  imports: [CommonModule, AdvertisementRoutingModule],
+  imports: [CommonModule, AdvertisementRoutingModule, MaterialModule],
 })
 export class AdvertisementModule {}
