6e168ec25bebea5f25a77cb9c788a8310b26af2e
Paolo Tabacco <tabaccopaolo99@gmail.com>
2020-10-27 10:31:31 +0100

    Created photogallery component

--- src/app/modules/core/advertisement/advertisement.module.ts
+++ src/app/modules/core/advertisement/advertisement.module.ts
@@ -6,8 +6,9 @@ import { AdvertisementRoutingModule } from './advertisement-routing.module'
 import { AdvertisementDetailsComponent } from './components/advertisement-details/advertisement-details.component'
 import { AdvertisementListComponent } from './components/advertisement-list/advertisement-list.component'
 import { CardContainerComponent } from './components/advertisement-list/properties-container/card-container/card-container.component'
-import { PropertiesContainerComponent } from './components/advertisement-list/properties-container/properties-container.component';
 import { CardMainViewComponent } from './components/advertisement-list/properties-container/card-container/card-main-view/card-main-view.component'
+import { PropertiesContainerComponent } from './components/advertisement-list/properties-container/properties-container.component';
+import { PhotogalleryComponent } from './components/advertisement-list/properties-container/card-container/card-main-view/photogallery/photogallery.component'
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { CardMainViewComponent } from './components/advertisement-list/propertie
     PropertiesContainerComponent,
     CardContainerComponent,
     CardMainViewComponent,
+    PhotogalleryComponent,
   ],
 
   imports: [CommonModule, AdvertisementRoutingModule],
