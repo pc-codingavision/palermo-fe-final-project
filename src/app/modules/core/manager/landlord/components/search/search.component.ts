969cdff786a3185f710c2409fefb7f8d04b90c56
Carmelo <melonemark8@gmail.com>
2020-10-28 11:58:28 +0100

    Fix search UI for landlord entity

--- src/app/modules/core/manager/landlord/components/search/search.component.ts
+++ src/app/modules/core/manager/landlord/components/search/search.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core'
 import { FormControl } from '@angular/forms'
+import { Observable } from 'rxjs'
+import { map, startWith } from 'rxjs/operators'
 import { Tenant } from 'src/app/shared/models/tenant'
 
-import { TENANTS } from './../../../../../../shared/models/mock-data/mock-tenant'
+import { Landlord } from './../../../../../../shared/models/landlord'
+import { LANDLORDS } from './../../../../../../shared/models/mock-data/mock-landlord'
 
 @Component({
   selector: 'cav-search',
@@ -10,17 +13,58 @@ import { TENANTS } from './../../../../../../shared/models/mock-data/mock-tenant
   styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
-  tenant: Tenant[] = []
-  tenants = this.tenant
-  nameControl = new FormControl()
-  emailControl = new FormControl()
-  phoneControl = new FormControl()
-  tenantsPhoneNumber = this.tenants.map((n) => n.phone)
+  landlords: Landlord[] = []
+  formControl = new FormControl()
+  filteredFullName: Observable<string[]>
+  filteredEmail: Observable<string[]>
+  filteredPhone: Observable<string[]>
   constructor() {}
 
   ngOnInit(): void {
-    TENANTS.forEach((v) => {
-      this.tenant.push(Tenant.Build(v))
+    LANDLORDS.forEach((v) => {
+      this.landlords.push(Tenant.Build(v))
     })
+
+    this.filteredFullName = this.formControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filterFullName(value))
+    )
+    this.filteredEmail = this.formControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filterEmail(value))
+    )
+    this.filteredPhone = this.formControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filterPhone(value))
+    )
+  }
+
+  private _filterFullName(value: string): string[] {
+    const filterValue = this._normalizeValue(value)
+    const landlordsUsername = this.landlords.map((landlord) => landlord.fullName)
+    return landlordsUsername.filter((tenant) =>
+      this._normalizeValue(tenant).includes(filterValue)
+    )
+  }
+
+  private _filterEmail(value: string): string[] {
+    const filterValue = this._normalizeValue(value)
+    const landlordsEmail = this.landlords.map((landlord) => landlord.mail)
+    return landlordsEmail.filter((tenant) =>
+      this._normalizeValue(tenant).includes(filterValue)
+    )
+  }
+
+  private _filterPhone(value: string): string[] {
+    const filterValue = this._normalizeValue(value)
+    const landlordsPhone = this.landlords.map((landlord) => landlord.phone[0].digits)
+    console.log(landlordsPhone)
+    return landlordsPhone.filter((tenant) =>
+      this._normalizeValue(tenant).includes(filterValue)
+    )
+  }
+
+  private _normalizeValue(value: string): string {
+    return value.toLowerCase().replace(/\s/g, '')
   }
 }
