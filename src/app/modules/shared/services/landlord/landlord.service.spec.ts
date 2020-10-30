012043da2616124d916f47efbe9867f8a21cf125
Cosimo Nigrelli <nigrellicosimo@gmail.com>
2020-10-30 12:23:44 +0100

    Fix style

--- src/app/modules/shared/services/landlord/landlord.service.spec.ts
+++ src/app/modules/shared/services/landlord/landlord.service.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing'
 
-import { LandlordService } from './landlord.service';
+import { LandlordService } from './landlord.service'
 
 describe('LandlordService', () => {
-  let service: LandlordService;
+  let service: LandlordService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(LandlordService);
-  });
+    TestBed.configureTestingModule({})
+    service = TestBed.inject(LandlordService)
+  })
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    expect(service).toBeTruthy()
+  })
+})
