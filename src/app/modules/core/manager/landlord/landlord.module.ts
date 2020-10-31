915dfc219f6c37d8cd6bc9f5f0ccc8c8a4cbb4c8
Davide081987 <davidetrovato87@gmail.com>
2020-10-31 16:07:59 +0100

    fixed error

--- src/app/modules/core/manager/landlord/landlord.module.ts
+++ src/app/modules/core/manager/landlord/landlord.module.ts
@@ -6,6 +6,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { MaterialModule } from './../../../shared/material.module'
 import { LandlordContainerComponent } from './components/container/landlord-container.component'
 import { AddressDetailsComponent } from './components/edit/components/address-details/address-details.component'
+import { ButtonsContainerComponent } from './components/edit/components/buttons-container/buttons-container.component'
 import { ContactDetailsComponent } from './components/edit/components/contact-details/contact-details.component'
 import { EditContainerComponent } from './components/edit/components/container/edit-container.component'
 import { PersonalDetailsComponent } from './components/edit/components/personal-details/personal-details.component'
@@ -19,8 +20,7 @@ import { ContactComponent } from './components/view/components/contact/contact.c
 import { ViewContainerComponent } from './components/view/components/container/container.component'
 import { PhoneComponent } from './components/view/components/phone/phone.component'
 import { StatusComponent } from './components/view/components/status/status.component'
-import { LandlordRoutingModule } from './landlord-routing.module';
-import { ButtonsContainerComponent } from './components/edit/components/buttons-container/buttons-container.component'
+import { LandlordRoutingModule } from './landlord-routing.module'
 
 const landlordComponents = [
   LandlordListComponent,
