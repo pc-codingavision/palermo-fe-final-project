13343499b0cefc1d473fe24780e3c2afa45dfe37
Cosimo Nigrelli <nigrellicosimo@gmail.com>
2020-10-31 18:45:35 +0100

    Display and get landlords from service

--- src/app/modules/core/manager/landlord/components/list/landlord-list.component.ts
+++ src/app/modules/core/manager/landlord/components/list/landlord-list.component.ts
@@ -1,6 +1,6 @@
 import { animate, state, style, transition, trigger } from '@angular/animations'
-import { Component, OnInit } from '@angular/core'
-import { Observable } from 'rxjs'
+import { Component, OnDestroy, OnInit } from '@angular/core'
+import { Subscription } from 'rxjs'
 import { LANDLORDS } from 'src/app/shared/models/mock-data/mock-landlord'
 
 import { Landlord } from './../../../../../../shared/models/landlord'
@@ -28,18 +28,22 @@ export interface Elements {
     ]),
   ],
 })
-export class LandlordListComponent implements OnInit {
+export class LandlordListComponent implements OnInit, OnDestroy {
   displayedColumns: string[] = ['id', 'fullName', 'mail', 'phone_number']
   expandedElement: Elements | null
   landlords: Landlord[]
+  private subscription: Subscription
 
   constructor(private landlordService: LandlordService) {}
 
-  getLandlords(): Observable<Landlord[]> {
-    return this.landlordService.getAllLandlords()
+  ngOnInit(): void {
+    const obs = this.landlordService.getAllLandlords()
+    this.subscription = obs.subscribe((landlord) => (this.landlords = landlord))
   }
 
-  ngOnInit(): void {}
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
 
   remove(landlord: Landlord): void {
     this.landlordService.deleteLandlord(landlord)
