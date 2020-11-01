12245ffa463773415ad1b80863224f72c3ac60e8
Ludovico Zitelli <ludovico.zitelli@gmail.com>
2020-11-01 12:31:18 +0100

    Added minPrice and maxPrice based on properties prices

--- src/app/modules/core/advertisement/advertisement.service.ts
+++ src/app/modules/core/advertisement/advertisement.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core'
 import { Observable, of } from 'rxjs'
+import { map } from 'rxjs/operators'
 import { IAdvertisement } from 'src/app/shared/models/advertisement'
 
 import { ADVERTISEMENTS } from '../../../shared/models/mock-data/mock-advertisement'
@@ -16,5 +17,23 @@ export class AdvertisementService {
 
   findById(id: number): Observable<IAdvertisement> {
     return of(ADVERTISEMENTS.find((advert) => (advert.id = id)))
+  }
+
+  returnMaxPriceProperty(): Observable<IAdvertisement[]> {
+    return of(ADVERTISEMENTS).pipe(
+      map((maxPriceProperty: IAdvertisement[]) => {
+        maxPriceProperty.sort((x, y) => y.price - x.price)
+        return maxPriceProperty.slice(0, 1)
+      })
+    )
+  }
+
+  returnMinPriceProperty(): Observable<IAdvertisement[]> {
+    return of(ADVERTISEMENTS).pipe(
+      map((minPriceProperty: IAdvertisement[]) => {
+        minPriceProperty.sort((x, y) => x.price - y.price)
+        return minPriceProperty.slice(0, 1)
+      })
+    )
   }
 }
--- src/app/modules/core/advertisement/components/advertisement-list/sidebar/price-filter/price-filter.component.ts
+++ src/app/modules/core/advertisement/components/advertisement-list/sidebar/price-filter/price-filter.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core'
+import { Advertisement } from 'src/app/shared/models/advertisement'
+
+import { AdvertisementService } from '../../../../advertisement.service'
 
 @Component({
   selector: 'cav-price-filter',
@@ -6,11 +9,38 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./price-filter.component.scss'],
 })
 export class PriceFilterComponent implements OnInit {
-  constructor() {}
+  maxPriceAdvertisement: Advertisement[]
+  minPriceAdvertisement: Advertisement[]
+  maxPrice: number
+  minPrice: number
+  constructor(private advertisementService: AdvertisementService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.maxPrice = this.returnMaxPrice()
+    this.minPrice = this.returnMinPrice()
+  }
 
   formatLabel(value: number): string {
     return value + '€'
+  }
+
+  returnMaxPrice(): number {
+    this.advertisementService
+      .returnMaxPriceProperty()
+      .subscribe(
+        (maxPriceAdvertisement) => (this.maxPriceAdvertisement = maxPriceAdvertisement)
+      )
+    const string = this.maxPriceAdvertisement.map((x) => x.price).toString()
+    return parseInt(string)
+  }
+
+  returnMinPrice(): number {
+    this.advertisementService
+      .returnMinPriceProperty()
+      .subscribe(
+        (minPriceAdvertisement) => (this.minPriceAdvertisement = minPriceAdvertisement)
+      )
+    const string = this.minPriceAdvertisement.map((x) => x.price).toString()
+    return parseInt(string)
   }
 }
