8e86cf1a5727c3f73bd9d0dfe6d3cc3efb3869d5
googlielmo93 <vincenzoguglielmo@gmail.com>
2020-11-02 10:16:55 +0100

    remove boolean unused in reset-password

--- src/app/modules/core/manager/landlord/components/edit/components/reset-password/reset-password.component.ts
+++ src/app/modules/core/manager/landlord/components/edit/components/reset-password/reset-password.component.ts
@@ -7,7 +7,6 @@ import { Component, Inject, OnInit } from '@angular/core'
   styleUrls: ['./reset-password.component.scss'],
 })
 export class ResetPasswordComponent implements OnInit {
-  isResetDialog: boolean
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
 
   ngOnInit(): void {}
