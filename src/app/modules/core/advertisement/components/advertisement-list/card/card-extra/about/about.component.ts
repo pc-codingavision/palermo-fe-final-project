b0665f097eb39a113bd621c4978cc15a0ca358da
Giulia Reas <56250206+lareas123@users.noreply.github.com>
2020-11-04 17:59:33 +0100

    deleted comments

--- src/app/modules/core/advertisement/components/advertisement-list/card/card-extra/about/about.component.ts
+++ src/app/modules/core/advertisement/components/advertisement-list/card/card-extra/about/about.component.ts
@@ -9,19 +9,8 @@ import { IProperty } from './../../../../../../../../shared/models/property'
 })
 export class AboutComponent implements OnInit {
   @Input() property: IProperty
+
   constructor() {}
 
   ngOnInit(): void {}
-
-  // getProperties(): Observable<Property[]> {
-  //   return this.propertiesService.findAll()
-  // }
-
-  // getPropertyById(id: number): Observable<Property> {
-  //   return this.propertiesService.findById(id)
-  // }
-
-  // getDescriptionById(): Observable<any> {
-  //   return this.propertyService.findById(1).pipe(map((property) => property.description))
-  // }
 }
