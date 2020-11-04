ccf05614e91073954bdfd4892a305e906f21f16c
Fanni Felfoldi <felfoldi26fanni@gmail.com>
2020-11-04 18:45:38 +0100

    add tests for propertyService

--- src/app/modules/core/advertisement/services/property.service.spec.ts
+++ src/app/modules/core/advertisement/services/property.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing'
 
+import { PROPERTIES_MOCK_DATA } from './../../../../shared/models/mock-data/data'
 import { PropertyService } from './property.service'
 
 describe('PropertyService', () => {
   let service: PropertyService
+
+  const mockProperty = PROPERTIES_MOCK_DATA[0]
 
   beforeEach(() => {
     TestBed.configureTestingModule({})
@@ -12,5 +15,19 @@ describe('PropertyService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy()
+  })
+
+  it('should return all properties', () => {
+    expect(
+      service
+        .findAll()
+        .subscribe((properties) => expect(properties).toEqual(PROPERTIES_MOCK_DATA))
+    )
+  })
+
+  it('should return specific property based on the passed id', () => {
+    expect(
+      service.findById(1).subscribe((property) => expect(property).toEqual(mockProperty))
+    )
   })
 })
