d2245dd8bd5f03797d3e243f4e1f81960afc76a1
Fanni Felfoldi <felfoldi26fanni@gmail.com>
2020-11-04 18:53:41 +0100

    cleared reviews variabile

--- src/app/modules/core/advertisement/components/advertisement-list/card/card-extra/review/review.component.ts
+++ src/app/modules/core/advertisement/components/advertisement-list/card/card-extra/review/review.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
+
+import { IProperty } from './../../../../../../../../shared/models/property'
 
 @Component({
   selector: 'cav-review',
@@ -6,20 +8,7 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./review.component.scss'],
 })
 export class ReviewComponent implements OnInit {
-  reviews = [
-    {
-      title: 'Good',
-      tenantId: 3,
-      description: 'Very spacious and accessible house',
-      vote: 4.5,
-    },
-    {
-      title: 'Not great',
-      tenantId: 4,
-      description: 'The hostel was very very chaotic ',
-      vote: 1.5,
-    },
-  ]
+  @Input() property: IProperty
 
   constructor() {}
 
