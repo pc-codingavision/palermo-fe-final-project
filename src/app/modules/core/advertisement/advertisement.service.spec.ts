a7fbc2c82e10c745e84f21cac439ec84854be0bf
Ludovico Zitelli <ludovico.zitelli@gmail.com>
2020-11-05 17:40:53 +0100

    Reworked service tests

--- src/app/modules/core/advertisement/advertisement.service.spec.ts
+++ src/app/modules/core/advertisement/advertisement.service.spec.ts
@@ -16,52 +16,42 @@ describe('AdvertisementService', () => {
   })
 
   it('should return all advertisements', () => {
-    expect(
-      service
-        .findAll()
-        .subscribe((advertisements) =>
-          expect(advertisements).toEqual(ADVERTISEMENTS_MOCK_DATA)
-        )
-    )
+    service
+      .findAll()
+      .subscribe((advertisements) =>
+        expect(advertisements).toEqual(ADVERTISEMENTS_MOCK_DATA)
+      )
   })
 
   it('should return specific advertisement based on the passed id', () => {
-    expect(
-      service
-        .findById(1)
-        .subscribe((advertisement) =>
-          expect(advertisement).toEqual(ADVERTISEMENTS_MOCK_DATA[0])
-        )
-    )
+    service
+      .findById(1)
+      .subscribe((advertisement) =>
+        expect(advertisement).toEqual(ADVERTISEMENTS_MOCK_DATA[0])
+      )
   })
 
   it('should return filtered advertisements', () => {
-    expect(
-      service
-        .returnPriceFilteredAdvertisements(20)
-        .subscribe((filteredAdvertisements) =>
-          expect(filteredAdvertisements).toEqual([ADVERTISEMENTS_MOCK_DATA[2]])
-        )
-    )
+    service
+      .returnPriceFilteredAdvertisements(20)
+      .subscribe((filteredAdvertisements) =>
+        expect(filteredAdvertisements).toEqual([ADVERTISEMENTS_MOCK_DATA[2]])
+      )
   })
 
   it('should return the highest price between advertisements', () => {
-    expect(
-      service
-        .findAdvertisementsHighestPrice()
-        .subscribe((filteredAdvertisementPrice) =>
-          expect(filteredAdvertisementPrice).toEqual(40)
-        )
-    )
+    service
+      .findAdvertisementsHighestPrice()
+      .subscribe((filteredAdvertisementPrice) =>
+        expect(filteredAdvertisementPrice).toEqual(40)
+      )
   })
 
   it('should return the lowest price between advertisements', () => {
-    expect(
-      service
-        .findAdvertisementsLowestPrice()
-        .subscribe((filteredAdvertisementPrice) =>
-          expect(filteredAdvertisementPrice).toEqual(20)
-        )
-    )
+    service
+      .findAdvertisementsLowestPrice()
+      .subscribe((filteredAdvertisementPrice) =>
+        expect(filteredAdvertisementPrice).toEqual(20)
+      )
   })
 })
