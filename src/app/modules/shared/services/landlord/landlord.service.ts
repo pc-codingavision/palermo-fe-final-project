8410142fea795d3058855ec15a5cf7127c1665e7
Carmelo <melonemark8@gmail.com>
2020-11-06 10:54:24 +0100

    Add landlord service and test

---
+++ src/app/modules/shared/services/landlord/landlord.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core'
+import { LANDLORDS_MOCK_DATA } from '@shared/models/mock-data/data'
+import { Observable, of } from 'rxjs'
+import { Landlord } from 'src/app/shared/models/landlord'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LandlordService {
+  landlords: Landlord[] = LANDLORDS_MOCK_DATA.map((landlord) => ({
+    ...landlord,
+  })) as Landlord[]
+
+  constructor() {}
+
+  private getArrayIndexById(id: number): number | null {
+    const index = this.landlords.findIndex((landlord) => landlord.id === id)
+    return index === -1 ? null : index
+  }
+
+  getAll(): Observable<Landlord[]> {
+    console.log(this.landlords)
+    return of(this.landlords.map((landlord) => Landlord.Build(landlord)))
+  }
+
+  getById(id: number): Observable<Landlord | null> {
+    if (id !== null) {
+      const landlordById = this.landlords.find((landlord) => landlord.id === id)
+      if (landlordById !== undefined) {
+        return of(landlordById)
+      }
+    }
+    return of(null)
+  }
+
+  delete(id: number): void {
+    if (this.getArrayIndexById(id) !== null) {
+      this.landlords.splice(this.getArrayIndexById(id), 1)
+    }
+  }
+
+  add(landlord: Landlord): void {
+    if (landlord !== null) {
+      this.landlords.push(landlord)
+    }
+  }
+
+  update(landlord: Landlord): Observable<Landlord | null> {
+    if (landlord !== null) {
+      const index = this.getArrayIndexById(landlord.id)
+      if (typeof index === 'number') {
+        return of((this.landlords[index] = landlord))
+      }
+    }
+    return of(null)
+  }
+
+  toggleStatus(id: number): Observable<Landlord | null> {
+    const index = this.getArrayIndexById(id)
+    if (typeof index === 'number') {
+      this.landlords[index].status = !this.landlords[index].status
+      return of(this.landlords[index])
+    }
+    return of(null)
+  }
+}
