7d7d0caa41aa5fba4b05faeb09ade14634f158e1
Fanni Felfoldi <felfoldi26fanni@gmail.com>
2020-11-07 13:07:17 +0100

    add @Input() to facilities.component

--- src/app/modules/core/advertisement/components/advertisement-list/card/card-extra/facilities/facilities.component.ts
+++ src/app/modules/core/advertisement/components/advertisement-list/card/card-extra/facilities/facilities.component.ts
@@ -2,7 +2,6 @@ import { Input } from '@angular/core'
 import { Component, OnInit } from '@angular/core'
 
 import { MockAdvertisement } from './../../../../../mock-advertisement/mock-advertisement'
-import { PropertyService } from './../../../../../services/property.service'
 
 @Component({
   selector: 'cav-facilities',
@@ -11,13 +10,7 @@ import { PropertyService } from './../../../../../services/property.service'
 })
 export class FacilitiesComponent implements OnInit {
   @Input() advertisement: MockAdvertisement
-  constructor(private propertyService: PropertyService) {}
+  constructor() {}
 
   ngOnInit(): void {}
-
-  /* getFacilitiesById(): Observable<IFacilities> {
-    return this.propertyService
-      .findById(1)
-      .pipe(map((property) => property.facilities.tv))
-  }*/
 }
