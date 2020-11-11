cb742bcbef156d3243813a7a9c9807be6c553692
Fanni Felfoldi <felfoldi26fanni@gmail.com>
2020-11-11 19:42:25 +0100

    fixed card-extra input

---
+++ src/app/modules/core/advertisement/components/card/card-bottom/card-extra/card-extra.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core'
+import { IMockAdvertisement } from '@modules/core/advertisement/mock-advertisement/mock-advertisement'
+
+@Component({
+  selector: 'cav-card-extra',
+  templateUrl: './card-extra.component.html',
+  styleUrls: ['./card-extra.component.scss'],
+})
+export class CardExtraComponent implements OnInit {
+  @Input() advertisement: IMockAdvertisement
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
