ca5b35aec8cb683854c49c53e94b46fad5ebd19c
Fanni Felfoldi <felfoldi26fanni@gmail.com>
2020-11-11 19:42:56 +0100

    fixed facilitie.component input

---
+++ src/app/modules/core/advertisement/components/card/card-bottom/facilities/facilities.component.ts
@@ -0,0 +1,15 @@
+import { Input } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { IFacilities } from '@shared/models/property'
+
+@Component({
+  selector: 'cav-facilities',
+  templateUrl: './facilities.component.html',
+  styleUrls: ['./facilities.component.scss'],
+})
+export class FacilitiesComponent implements OnInit {
+  @Input() facilities: IFacilities
+  constructor() {}
+
+  ngOnInit(): void {}
+}
