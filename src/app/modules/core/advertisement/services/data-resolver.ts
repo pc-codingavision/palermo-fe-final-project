b3ba55cdfb4dcf56c976a9659b6f70f8e286641f
Fanni Felfoldi <felfoldi26fanni@gmail.com>
2020-11-18 17:57:44 +0100

    fix data-resolver.ts

--- src/app/modules/core/advertisement/services/data-resolver.ts
+++ src/app/modules/core/advertisement/services/data-resolver.ts
@@ -1,22 +1,20 @@
 import { Injectable } from '@angular/core'
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router'
-import { Observable, of } from 'rxjs'
+import { AdvertisementService } from '@modules/core/advertisement/advertisement.service'
+import { Observable } from 'rxjs'
 
-import { MOCKADVERTISEMENTS_MOCK_DATA } from './../mock-advertisement/mock-advertisement'
 import { MockAdvertisement } from '../mock-advertisement/mock-advertisement'
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataResolver implements Resolve<MockAdvertisement[]> {
-  advertisements = MOCKADVERTISEMENTS_MOCK_DATA.map((advertisement) =>
-    MockAdvertisement.Build(advertisement)
-  )
-  constructor() {}
+  constructor(private advertisementService: AdvertisementService) {}
+
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<MockAdvertisement[]> {
-    return of(this.advertisements)
+    return this.advertisementService.findAll()
   }
 }
