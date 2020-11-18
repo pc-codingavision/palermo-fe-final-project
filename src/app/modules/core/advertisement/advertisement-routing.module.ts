822c06dfa01014c1fb743a8ab54ca1fa5fa08306
Fanni Felfoldi <felfoldi26fanni@gmail.com>
2020-11-18 13:28:20 +0100

    add resolve to the advertisementList path

--- src/app/modules/core/advertisement/advertisement-routing.module.ts
+++ src/app/modules/core/advertisement/advertisement-routing.module.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router'
 import { AdvertisementContainerComponent } from '@modules/core/advertisement/components/advertisement-container/advertisement-container.component'
 import { AdvertisementDetailsComponent } from '@modules/core/advertisement/components/advertisement-details/advertisement-details.component'
 import { AdvertisementListComponent } from '@modules/core/advertisement/components/advertisement-list/advertisement-list.component'
+import { DataResolver } from '@modules/core/advertisement/services/data-resolver'
 
 export const advertisementRoutes: Routes = [
   { path: '', redirectTo: 'advertisement', pathMatch: 'full' },
   { path: 'advertisement', component: AdvertisementContainerComponent },
-  { path: 'advertisement/list', component: AdvertisementListComponent },
+  {
+    path: 'advertisement/list',
+    component: AdvertisementListComponent,
+    resolve: { result: DataResolver },
+  },
   { path: 'advertisement/details/:id', component: AdvertisementDetailsComponent },
 ]
 
