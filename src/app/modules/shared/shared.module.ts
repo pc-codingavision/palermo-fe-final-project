37f5a4bb6e69b193027f5ab5fdb65547be4943d4
Cosimo Nigrelli <nigrellicosimo@gmail.com>
2020-11-20 11:01:53 +0100

    Add: create spinner component

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,13 +3,14 @@ import { FlexLayoutModule } from '@angular/flex-layout'
 import { ReactiveFormsModule } from '@angular/forms'
 import { MaterialModule } from '@modules/shared/material.module'
 import { DialogComponent } from '@shared/components/dialog/dialog.component'
+import { SpinnerComponent } from '@shared/components/spinner/spinner.component'
 
-const sharedComponents = [DialogComponent]
+const sharedComponents = [DialogComponent, SpinnerComponent]
 const sharedModules = [FlexLayoutModule, MaterialModule, ReactiveFormsModule]
 
 @NgModule({
   declarations: [...sharedComponents],
   imports: [...sharedModules],
-  exports: [...sharedModules],
+  exports: [...sharedModules, SpinnerComponent],
 })
 export class SharedModule {}
