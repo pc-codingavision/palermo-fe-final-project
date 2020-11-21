3ec86b6f5e3d76a05be7e61c76d9de7b7bb9d2ec
Cosimo Nigrelli <nigrellicosimo@gmail.com>
2020-11-21 13:40:41 +0100

    Add: imported common module to shared module.

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -4,13 +4,19 @@ import { ReactiveFormsModule } from '@angular/forms'
 import { MaterialModule } from '@modules/shared/material.module'
 import { DialogComponent } from '@shared/components/dialog/dialog.component'
 import { SpinnerComponent } from '@shared/components/spinner/spinner.component'
+import { CommonModule } from '@angular/common'
 
 const sharedComponents = [DialogComponent, SpinnerComponent]
-const sharedModules = [FlexLayoutModule, MaterialModule, ReactiveFormsModule]
+const sharedModules = [
+  FlexLayoutModule,
+  MaterialModule,
+  ReactiveFormsModule,
+  CommonModule,
+]
 
 @NgModule({
   declarations: [...sharedComponents],
   imports: [...sharedModules],
-  exports: [...sharedModules, SpinnerComponent],
+  exports: [...sharedModules, ...sharedComponents],
 })
 export class SharedModule {}
