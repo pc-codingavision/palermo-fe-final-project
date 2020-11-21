ed5d3cc098c120f20187df1106d2d60d6fcbe141
Ludovico Zitelli <ludovico.zitelli@gmail.com>
2020-11-21 23:13:05 +0100

    Added unsubscription

--- src/app/modules/core/advertisement/components/advertisement-container/advertisement-container.component.ts
+++ src/app/modules/core/advertisement/components/advertisement-container/advertisement-container.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
 import { AdvertisementService } from '@modules/core/advertisement/advertisement.service'
 import { MockAdvertisement } from '@modules/core/advertisement/mock-advertisement/mock-advertisement'
 import { IFacilities } from '@shared/models/property'
 import * as _ from 'lodash'
-import { Observable } from 'rxjs'
+import { Observable, Subscription } from 'rxjs'
 
 @Component({
   selector: 'cav-advertisement-container',
   templateUrl: './advertisement-container.component.html',
   styleUrls: ['./advertisement-container.component.scss'],
 })
-export class AdvertisementContainerComponent implements OnInit {
+export class AdvertisementContainerComponent implements OnInit, OnDestroy {
   minPrice$: Observable<number>
   maxPrice$: Observable<number>
   advertisements$: Observable<MockAdvertisement[]>
+  subscription: Subscription
 
   constructor(private advertisementService: AdvertisementService) {}
 
@@ -21,24 +22,30 @@ export class AdvertisementContainerComponent implements OnInit {
     this.advertisements$ = this.advertisementService.findAll()
     this.minPrice$ = this.getMinPrice()
     this.maxPrice$ = this.getMaxPrice()
-    this.advertisementService.sidebarFilter.subscribe((filter: number | IFacilities) => {
-      if (_.isObject(filter)) {
-        this.advertisements$ = this.advertisementService.getAdvertisementsFilteredByFacilities(
-          filter
-        )
-      }
-      if (_.isNumber(filter)) {
-        if (filter <= 5) {
-          this.advertisements$ = this.advertisementService.getAdvertisementsFilteredByScore(
-            filter
-          )
-        } else {
-          this.advertisements$ = this.advertisementService.getAdvertisementsFilteredByPrice(
+    this.subscription = this.advertisementService.sidebarFilter.subscribe(
+      (filter: number | IFacilities) => {
+        if (_.isObject(filter)) {
+          this.advertisements$ = this.advertisementService.getAdvertisementsFilteredByFacilities(
             filter
           )
         }
+        if (_.isNumber(filter)) {
+          if (filter <= 5) {
+            this.advertisements$ = this.advertisementService.getAdvertisementsFilteredByScore(
+              filter
+            )
+          } else {
+            this.advertisements$ = this.advertisementService.getAdvertisementsFilteredByPrice(
+              filter
+            )
+          }
+        }
       }
-    })
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
   }
 
   getMinPrice(): Observable<number> {
