fbee6855720ee36fe565bde82a9448a369f6492a
Cosimo Nigrelli <nigrellicosimo@gmail.com>
2020-11-21 14:05:22 +0100

    Fix: common module import.

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,10 +1,10 @@
+import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { FlexLayoutModule } from '@angular/flex-layout'
 import { ReactiveFormsModule } from '@angular/forms'
 import { MaterialModule } from '@modules/shared/material.module'
 import { DialogComponent } from '@shared/components/dialog/dialog.component'
 import { SpinnerComponent } from '@shared/components/spinner/spinner.component'
-import { CommonModule } from '@angular/common'
 
 const sharedComponents = [DialogComponent, SpinnerComponent]
 const sharedModules = [
