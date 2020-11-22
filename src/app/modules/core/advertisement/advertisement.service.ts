8def8e7f4e441caff6d9a1063d68a6bf7a913af5
Giulia Reas <56250206+lareas123@users.noreply.github.com>
2020-11-22 19:19:53 +0100

    bug fix

--- src/app/modules/core/advertisement/advertisement.service.ts
+++ src/app/modules/core/advertisement/advertisement.service.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import {
   MOCKADVERTISEMENTS_MOCK_DATA,
@@ -19,7 +18,7 @@ export class AdvertisementService {
     MockAdvertisement.Build(advertisement)
   )
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   findAll(): Observable<MockAdvertisement[]> {
     return of(this.advertisements)
