887b75a9e5dc4c758de10d32c19b609f1b4e8685
Ludovico Zitelli <ludovico.zitelli@gmail.com>
2020-11-23 20:28:30 +0100

    Fixed style

--- src/app/modules/core/advertisement/services/sidebar.service.spec.ts
+++ src/app/modules/core/advertisement/services/sidebar.service.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing'
 
-import { SidebarService } from './sidebar.service';
+import { SidebarService } from './sidebar.service'
 
 describe('SidebarService', () => {
-  let service: SidebarService;
+  let service: SidebarService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SidebarService);
-  });
+    TestBed.configureTestingModule({})
+    service = TestBed.inject(SidebarService)
+  })
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    expect(service).toBeTruthy()
+  })
+})
