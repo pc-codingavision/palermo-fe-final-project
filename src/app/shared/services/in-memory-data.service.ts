06fd2acdcde4e1169b11ec6059e7256405f0eb17
Giulia Reas <56250206+lareas123@users.noreply.github.com>
2020-11-24 12:27:47 +0100

    modified const advertisements

--- src/app/shared/services/in-memory-data.service.ts
+++ src/app/shared/services/in-memory-data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core'
 import { IAdvertisement } from '@shared/models/advertisement'
+import { ADVERTISEMENTS_MOCK_DATA } from '@shared/models/mock-data/data'
 import { InMemoryDbService } from 'angular-in-memory-web-api'
 
 @Injectable({
@@ -7,62 +8,7 @@ import { InMemoryDbService } from 'angular-in-memory-web-api'
 })
 export class InMemoryDataService implements InMemoryDbService {
   createDb(): any {
-    const advertisements = [
-      {
-        id: 1,
-        landlordId: 1,
-        propertyId: 1,
-        reviews: [
-          {
-            title: 'Fantastic vacation',
-            tenantId: 1,
-            description: 'Fantastic vacation. Perfect house',
-            vote: 4.5,
-          },
-          {
-            title: 'Good',
-            tenantId: 2,
-            description: 'Nice house, very very clean',
-            vote: 4,
-          },
-        ],
-        price: 40,
-      },
-      {
-        id: 2,
-        landlordId: 2,
-        propertyId: 2,
-        reviews: [
-          {
-            title: 'Good',
-            tenantId: 2,
-            description: 'Very spacious and accessible house',
-            vote: 4.5,
-          },
-        ],
-        price: 30,
-      },
-      {
-        id: 3,
-        landlordId: 3,
-        propertyId: 3,
-        reviews: [
-          {
-            title: 'Not great',
-            tenantId: 1,
-            description: 'The hostel was very very chaotic',
-            vote: 1.5,
-          },
-          {
-            title: 'Terrible',
-            tenantId: 2,
-            description: 'Very busy and people without masks',
-            vote: 2,
-          },
-        ],
-        price: 20,
-      },
-    ]
+    const advertisements = ADVERTISEMENTS_MOCK_DATA
     return { advertisements }
   }
 
