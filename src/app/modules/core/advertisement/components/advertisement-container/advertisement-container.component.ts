d0c97becd7faefa07a0fd531cd7be56334e60c2a
Gabriele Bilello <gabriele.bilello1@gmail.com>
2020-11-24 11:44:22 +0100

    Created interface IReviewConfig, Created input config, Refactor mat-icon

--- src/app/modules/core/advertisement/components/advertisement-container/advertisement-container.component.ts
+++ src/app/modules/core/advertisement/components/advertisement-container/advertisement-container.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { AdvertisementService } from '@modules/core/advertisement/advertisement.service'
 import { MockAdvertisement } from '@modules/core/advertisement/mock-advertisement/mock-advertisement'
+import { ADVERTISEMENTS_MOCK_DATA } from '@shared/models/mock-data/data'
 import { Observable } from 'rxjs'
 
 @Component({
@@ -10,6 +11,7 @@ import { Observable } from 'rxjs'
 })
 export class AdvertisementContainerComponent implements OnInit {
   advertisements$: Observable<MockAdvertisement[]>
+  review = ADVERTISEMENTS_MOCK_DATA[0].reviews[0]
 
   constructor(private advertisementService: AdvertisementService) {}
 
--- src/app/modules/core/advertisement/components/card/card-bottom/review/review.component.ts
+++ src/app/modules/core/advertisement/components/card/card-bottom/review/review.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core'
+import { FormControl } from '@angular/forms'
 import { IMockReview } from '@modules/core/advertisement/mock-advertisement/mock-advertisement'
 
 @Component({
@@ -7,12 +8,30 @@ import { IMockReview } from '@modules/core/advertisement/mock-advertisement/mock
   styleUrls: ['./review.component.scss'],
 })
 export class ReviewComponent implements OnInit {
-  constructor() {}
   @Input() review: IMockReview
-
+  @Input() config: IReviewConfig
   score: number
+  title = new FormControl(this.review?.title)
+  description = new FormControl(this.review?.description)
+
+  constructor() {}
 
   ngOnInit(): void {
     this.score = Math.round(this.review?.vote)
   }
+
+  counter(i: number): number[] {
+    return new Array(i)
+  }
+
+  getValue(value: number): void {
+    console.log(value)
+  }
+}
+
+export interface IReviewConfig {
+  writable: boolean
+  minScore: number
+  maxScore: number
+  scoreSymbol: string
 }
