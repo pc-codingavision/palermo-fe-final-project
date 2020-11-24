2158b0668a6a15a31978c1c4d530762819783e24
Fanni Felfoldi <felfoldi26fanni@gmail.com>
2020-11-24 13:40:16 +0100

    add error handling

--- src/app/modules/core/advertisement/services/advertisement-resolver.ts
+++ src/app/modules/core/advertisement/services/advertisement-resolver.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core'
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router'
 import { AdvertisementService } from '@modules/core/advertisement/advertisement.service'
 import { MockAdvertisement } from '@modules/core/advertisement/mock-advertisement/mock-advertisement.ts'
-import { Observable } from 'rxjs'
+import { Observable, of } from 'rxjs'
+import { catchError, tap } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root',
@@ -14,6 +15,20 @@ export class AdvertisementResolver implements Resolve<MockAdvertisement[]> {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<MockAdvertisement[]> {
-    return this.advertisementService.findAll()
+    return this.advertisementService.findAll().pipe(
+      tap((_) => this.log('error')),
+      catchError(this.handleError('resolve', []))
+    )
+  }
+  log(arg0: string): void {
+    throw new Error('Method not implemented.')
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error('error')
+      this.log(`${operation} failed: ${error.message}`)
+      return of(result as T)
+    }
   }
 }
