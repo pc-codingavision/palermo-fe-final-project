e364a9824cf9ab2c75581ed735d96f192bd6398d
Cosimo Nigrelli <nigrellicosimo@gmail.com>
2020-11-25 11:25:35 +0100

    Added unit tests for spinner service.

--- src/app/shared/services/spinner.service.spec.ts
+++ src/app/shared/services/spinner.service.spec.ts
@@ -13,4 +13,14 @@ describe('SpinnerService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy()
   })
+
+  it('#showSpinner should show the spinner', () => {
+    service.showSpinner()
+    service.spinnerStatus.subscribe((val) => expect(val).toBeTrue())
+  })
+
+  it('#hideSpinner should hide the spinner', () => {
+    service.hideSpinner()
+    service.spinnerStatus.subscribe((val) => expect(val).toBeFalse())
+  })
 })
