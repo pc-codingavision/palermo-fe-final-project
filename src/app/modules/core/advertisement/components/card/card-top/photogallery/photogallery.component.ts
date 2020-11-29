1b7697d70a0305395b88d11be0007c0bd19225cb
Carmelo <melonemark8@gmail.com>
2020-11-29 18:38:17 +0100

    Refactor methods

--- src/app/modules/core/advertisement/components/card/card-top/photogallery/photogallery.component.ts
+++ src/app/modules/core/advertisement/components/card/card-top/photogallery/photogallery.component.ts
@@ -7,17 +7,20 @@ import { IPicture } from '@shared/models/property'
   styleUrls: ['./photogallery.component.scss'],
 })
 export class PhotogalleryComponent implements OnInit {
-  @Input() images: Array<IPicture>
+  @Input() images: IPicture[]
   constructor() {}
 
   ngOnInit(): void {
-    if (this.images !== undefined && !this?.images?.length) {
-      this.images[0] = {
-        image: 'https://cdn.pixabay.com/photo/2017/03/20/20/59/home-2160318_960_720.png',
-        thumbImage:
-          'https://cdn.pixabay.com/photo/2017/03/20/20/59/home-2160318_960_720.png',
-        alt: 'No image',
-      }
+    if (!this?.images?.length) {
+      this.images = [
+        {
+          image:
+            'https://cdn.pixabay.com/photo/2017/03/20/20/59/home-2160318_960_720.png',
+          thumbImage:
+            'https://cdn.pixabay.com/photo/2017/03/20/20/59/home-2160318_960_720.png',
+          alt: 'No image',
+        },
+      ]
     }
   }
 }
--- src/app/modules/core/advertisement/components/latest/latest-mini-card/latest-mini-card.component.ts
+++ src/app/modules/core/advertisement/components/latest/latest-mini-card/latest-mini-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core'
 import { IMockAdvertisement } from '@modules/core/advertisement/mock-advertisement/mock-advertisement'
+import { IPicture } from '@shared/models/property'
 
 @Component({
   selector: 'cav-latest-mini-card',
@@ -8,20 +9,20 @@ import { IMockAdvertisement } from '@modules/core/advertisement/mock-advertiseme
 })
 export class LatestMiniCardComponent implements OnInit {
   @Input() advertisement: IMockAdvertisement
-
+  @Input() images: IPicture[]
   constructor() {}
 
   ngOnInit(): void {
-    if (
-      this.advertisement !== undefined &&
-      !this.advertisement?.property?.images?.length
-    ) {
-      this.advertisement.property.images[0] = {
-        image: 'https://cdn.pixabay.com/photo/2017/03/20/20/59/home-2160318_960_720.png',
-        thumbImage:
-          'https://cdn.pixabay.com/photo/2017/03/20/20/59/home-2160318_960_720.png',
-        alt: 'No image',
-      }
+    if (!this.images?.length) {
+      this.images = [
+        {
+          image:
+            'https://cdn.pixabay.com/photo/2017/03/20/20/59/home-2160318_960_720.png',
+          thumbImage:
+            'https://cdn.pixabay.com/photo/2017/03/20/20/59/home-2160318_960_720.png',
+          alt: 'No image',
+        },
+      ]
     }
   }
 }
