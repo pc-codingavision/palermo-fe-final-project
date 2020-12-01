310802ad564a856213b47530539a2fa2de20b0cc
Matteo Oliva <olivamatteus@gmail.com>
2020-12-02 00:24:08 +0100

    Add test configuration for TenantService

--- src/app/modules/shared/services/tenant/in-memory-tenant.service.spec.ts
+++ src/app/modules/shared/services/tenant/in-memory-tenant.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { TestBed } from '@angular/core/testing'
 
 import { InMemoryTenantService } from './in-memory-tenant.service'
@@ -6,7 +7,9 @@ describe('InMemoryTenantService', () => {
   let service: InMemoryTenantService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({})
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    })
     service = TestBed.inject(InMemoryTenantService)
   })
 
