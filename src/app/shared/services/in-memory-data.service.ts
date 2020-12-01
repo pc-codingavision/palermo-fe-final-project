b2e76b9f0051dd87c3fafa858ed06ccc6b58a34b
Matteo Oliva <olivamatteus@gmail.com>
2020-12-01 17:36:23 +0100

    Added Tenant support and extended genId method with generic for both IAdvertisement and IUser

--- src/app/shared/services/in-memory-data.service.ts
+++ src/app/shared/services/in-memory-data.service.ts
@@ -3,19 +3,22 @@ import { IAdvertisement } from '@shared/models/advertisement'
 import { ADVERTISEMENTS_MOCK_DATA } from '@shared/models/mock-data/data'
 import { InMemoryDbService } from 'angular-in-memory-web-api'
 
+import { TENANTS_MOCK_DATA } from './../models/mock-data/data'
+import { Tenant } from './../models/tenant'
+import { IUser } from './../models/users'
+
 @Injectable({
   providedIn: 'root',
 })
 export class InMemoryDataService implements InMemoryDbService {
   createDb(): any {
     const advertisements = ADVERTISEMENTS_MOCK_DATA
-    return { advertisements }
+    const tenants = TENANTS_MOCK_DATA.map((tenant) => Tenant.Build(tenant))
+    return { advertisements, tenants }
   }
 
-  genId(advertisements: IAdvertisement[]): number {
-    return advertisements.length > 0
-      ? Math.max(...advertisements.map((adv) => adv.id)) + 1
-      : 1
+  genId<T extends IAdvertisement | IUser>(myTable: T[]): number {
+    return myTable.length > 0 ? Math.max(...myTable.map((t) => t.id)) + 1 : 1
   }
 
   constructor() {}
