c9c73da0c967b4d9ba181b5f3e4d3b60de5fbd25
Giulia Reas <56250206+giuliaReas@users.noreply.github.com>
2020-12-06 12:13:22 +0100

    refactoring tests

--- src/app/modules/shared/services/landlord/landlord.service.spec.ts
+++ src/app/modules/shared/services/landlord/landlord.service.spec.ts
@@ -1,137 +1,178 @@
+import { HttpClient, HttpResponse } from '@angular/common/http'
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing'
 import { TestBed } from '@angular/core/testing'
 import { PhoneType, Role } from '@shared/enum/enums'
+import { Landlord } from '@shared/models/landlord'
 import { LANDLORDS_MOCK_DATA } from '@shared/models/mock-data/data'
-import { Landlord } from 'src/app/shared/models/landlord'
 
 import { LandlordService } from './landlord.service'
 
 describe('LandlordService', () => {
+  let httpTestingController: HttpTestingController
   let service: LandlordService
-  let buildLandlords: Landlord[]
-  const landlordUpdate: Landlord = {
-    id: 1,
-    name: { firstName: 'Pino', surname: 'Cascio' },
-    phone: [
-      { id: 1, type: PhoneType.Mobile, digits: '3454545' },
-      { id: 2, type: PhoneType.Home, digits: '09252525' },
-    ],
-    mail: 'piero-landlord@test.com',
-    picture: 'https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg',
-    username: 'landlord1',
-    password: 'password',
-    status: true,
-    dateOfBirth: new Date(1980, 10, 10),
-    role: Role.Landlord,
-    address: {
-      line1: 'Via delle Magnolie',
-      city: 'Palermo',
-      state: 'italia',
-      postCode: '90100',
-    },
-    fullName: '',
-  }
-  const defaultLandlord = {
-    id: null,
-    name: { firstName: '', middleName: '', surname: '' },
-    phone: [],
-    mail: '',
-    picture: '',
-    username: '',
-    password: '',
-    status: false,
-    dateOfBirth: null,
-    role: Role.Landlord,
-    address: {
-      line1: '',
-      line2: '',
-      city: '',
-      state: '',
-      postCode: '',
-    },
-    fullName: '',
-  }
+  let httpClient: HttpClient
+  const buildLandlords: Landlord[] = LANDLORDS_MOCK_DATA.map((landlord) =>
+    Landlord.Build(landlord)
+  ) as Landlord[]
 
   beforeEach(() => {
-    TestBed.configureTestingModule({})
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [LandlordService],
+    })
+    httpClient = TestBed.inject(HttpClient)
+    httpTestingController = TestBed.inject(HttpTestingController)
     service = TestBed.inject(LandlordService)
-    buildLandlords = LANDLORDS_MOCK_DATA.map((landlord) =>
-      Landlord.Build(landlord)
-    ) as Landlord[]
   })
 
-  it('should be created', () => {
-    expect(service).toBeTruthy()
+  describe('#getAll', () => {
+    it('should return all landlords', () => {
+      service.getAll().subscribe((landlords) => expect(landlords).toEqual(buildLandlords))
+
+      const req = httpTestingController.expectOne(service.landlordsUrl)
+      expect(req.request.method).toEqual('GET')
+
+      req.flush(buildLandlords)
+    })
+
+    it("should return error 404 if get call don't work", () => {
+      const msg = 'deliberate 404 error'
+      service.getAll().subscribe((landlords) => expect(landlords?.length).toBeUndefined())
+
+      const req = httpTestingController.expectOne(service.landlordsUrl)
+      req.flush(msg, { status: 404, statusText: 'Not Found' })
+    })
   })
 
-  it('#getAll should return all landlords', (done: DoneFn) => {
-    service.getAll().subscribe((landlords) => expect(landlords).toEqual(buildLandlords))
-    done()
+  describe('#getById', () => {
+    it('should return landlord with specified id', () => {
+      service
+        .getById(1)
+        .subscribe((landlord) => expect(landlord).toEqual(buildLandlords[0]))
+
+      const req = httpTestingController.expectOne(`${service.landlordsUrl}/1`)
+      expect(req.request.method).toEqual('GET')
+
+      req.flush(buildLandlords[0])
+    })
   })
 
-  it('#getById should return landlord by id', (done: DoneFn) => {
-    service
-      .getById(1)
-      .subscribe((landlord) => expect(landlord).toEqual(buildLandlords[0]))
-    done()
+  describe('#delete', () => {
+    it('should remove landlord with specified id', () => {
+      service.delete(1).subscribe()
+      const delReq = httpTestingController.expectOne(`${service.landlordsUrl}/1`)
+      expect(delReq.request.method).toEqual('DELETE')
+    })
   })
 
-  it('#getdById should return null for not existing id or null', (done: DoneFn) => {
-    service.getById(5).subscribe((landlord) => expect(landlord).toBeNull())
-    service.getById(null).subscribe((landlord) => expect(landlord).toBeNull())
-    done()
+  describe('#add', () => {
+    const newLandlord: Landlord = {
+      id: 4,
+      name: { firstName: 'Mario', surname: 'Rossi' },
+      phone: [
+        { id: 1, type: PhoneType.Mobile, digits: '3454545' },
+        { id: 2, type: PhoneType.Home, digits: '09252525' },
+      ],
+      mail: 'piero-landlord@test.com',
+      picture: 'https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg',
+      username: 'landlord1',
+      password: 'password',
+      status: true,
+      dateOfBirth: new Date(1980, 10, 10),
+      role: Role.Landlord,
+      address: {
+        line1: 'Via delle Magnolie',
+        city: 'Palermo',
+        state: 'italia',
+        postCode: '90100',
+      },
+      fullName: 'Mario Rossi',
+    }
+    it('should add a landlord and return it', () => {
+      service
+        .add(newLandlord)
+        .subscribe((landlord) => expect(landlord).toEqual(newLandlord))
+      const req = httpTestingController.expectOne(service.landlordsUrl)
+      expect(req.request.method).toEqual('POST')
+      req.flush(newLandlord)
+    })
   })
+  describe('#update', () => {
+    const landlordUpdated: Landlord = {
+      id: 1,
+      name: { firstName: 'pino', surname: 'Cascio' },
+      phone: [
+        { id: 1, type: PhoneType.Mobile, digits: '3454545' },
+        { id: 2, type: PhoneType.Home, digits: '09252525' },
+      ],
+      mail: 'piero-landlord@test.com',
+      picture: 'https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg',
+      username: 'landlord1',
+      password: 'password',
+      status: true,
+      dateOfBirth: new Date(1980, 10, 10),
+      role: Role.Landlord,
+      address: {
+        line1: 'Via delle Magnolie',
+        city: 'Palermo',
+        state: 'italia',
+        postCode: '90100',
+      },
+      fullName: 'dfgdfg',
+    }
 
-  it('#deleteLandlord should delete landlord', () => {
-    expect(service.landlords.length).toBe(3)
-    service.delete(1)
-    expect(service.landlords.length).toBe(2)
-    expect(service.landlords).not.toContain(buildLandlords[0])
+    it('should modify landlord and return it updated', () => {
+      service.update(landlordUpdated).subscribe((a) => expect(a).toEqual(landlordUpdated))
+      const req = httpTestingController.expectOne(`${service.landlordsUrl}`)
+      expect(req.request.method).toEqual('PUT')
+      expect(req.request.body).toEqual(landlordUpdated)
+      const resp = new HttpResponse({
+        status: 200,
+        statusText: 'OK',
+        body: landlordUpdated,
+      })
+      req.event(resp)
+    })
   })
-
-  it("#delete shouldn't delete a landarlord for not existing id", () => {
-    expect(service.landlords.length).toBe(3)
-    service.delete(6)
-    expect(service.landlords.length).toBe(3)
-  })
-
-  it('#add should add a landlord', () => {
-    expect(service.landlords.length).toBe(3)
-    service.add(defaultLandlord)
-    expect(service.landlords.length).toBe(4)
-    expect(service.landlords).toContain(defaultLandlord)
-  })
-
-  it("#add shouldn't add if we don't pass Landlord ", () => {
-    expect(service.landlords.length).toBe(3)
-    service.add(null)
-    expect(service.landlords.length).toBe(3)
-  })
-
-  it('#update should update a landlord and return it', (done: DoneFn) => {
-    service
-      .update(landlordUpdate)
-      .subscribe(() => expect(service.landlords).not.toEqual(LANDLORDS_MOCK_DATA))
-    done()
-  })
-
-  it("#update shouldn't update if we don't pass landlord", (done: DoneFn) => {
-    service
-      .update(null)
-      .subscribe(() => expect(service.landlords).toEqual(LANDLORDS_MOCK_DATA))
-    done()
-  })
-
-  it('#toggleStatus should change the Landlord status', (done: DoneFn) => {
-    const status = service.landlords[0].status
-    service
-      .toggleStatus(1)
-      .subscribe(() => expect(service.landlords[0].status).not.toEqual(status))
-    done()
-  })
-
-  it('#genId should generate a new id', () => {
-    expect(service.genId()).toEqual(4)
-    service.landlords = []
-    expect(service.genId()).toEqual(1)
+  describe('#toggleStatus', () => {
+    const landlordUpdated: Landlord = {
+      id: 1,
+      name: { firstName: 'pino', surname: 'Cascio' },
+      phone: [
+        { id: 1, type: PhoneType.Mobile, digits: '3454545' },
+        { id: 2, type: PhoneType.Home, digits: '09252525' },
+      ],
+      mail: 'piero-landlord@test.com',
+      picture: 'https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg',
+      username: 'landlord1',
+      password: 'password',
+      status: true,
+      dateOfBirth: new Date(1980, 10, 10),
+      role: Role.Landlord,
+      address: {
+        line1: 'Via delle Magnolie',
+        city: 'Palermo',
+        state: 'italia',
+        postCode: '90100',
+      },
+      fullName: 'dfgdfg',
+    }
+    it('should change landlord status to false', () => {
+      service
+        .toggleStatus(landlordUpdated)
+        .subscribe((landlord: Landlord) => expect(landlord.status).toBeFalse)
+      const req = httpTestingController.expectOne(`${service.landlordsUrl}`)
+      expect(req.request.method).toEqual('PATCH')
+      expect(req.request.body).toBeFalse
+      const resp = new HttpResponse({
+        status: 200,
+        statusText: 'OK',
+        body: 'false',
+      })
+      req.event(resp)
+    })
   })
 })
