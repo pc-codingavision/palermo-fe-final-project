d692417cbb3cf01d959158eaf8027281c8dc211f
Giulia Reas <56250206+giuliaReas@users.noreply.github.com>
2020-12-06 12:12:45 +0100

    refacoring landlord service

--- src/app/modules/shared/services/landlord/landlord.service.ts
+++ src/app/modules/shared/services/landlord/landlord.service.ts
@@ -1,68 +1,16 @@
-import { Injectable } from '@angular/core'
-import { concat, Observable, of } from 'rxjs'
-import { Landlord } from 'src/app/shared/models/landlord'
 import { HttpClient } from '@angular/common/http'
-import { catchError, map, tap } from 'rxjs/operators'
+import { Injectable } from '@angular/core'
+import { Landlord } from '@shared/models/landlord'
+import { Observable, of } from 'rxjs'
+import { catchError, map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root',
 })
 export class LandlordService {
-  // landlords: Landlord[] = LANDLORDS_MOCK_DATA.map((landlord) => ({
-  //   ...landlord,
-  // })) as Landlord[]
+  landlordsUrl = 'api/landlords'
 
-  LANDLORD_ENDPOINT = 'api/landlords'
-
-  constructor(private httpClient: HttpClient) {}
-
-  // private getArrayIndexById(id: number): number | null {
-  //   const index = this.landlords.findIndex((landlord) => landlord.id === id)
-  //   return index === -1 ? null : index
-  // }
-
-  getAll(): Observable<Landlord[]> {
-    return this.httpClient
-      .get<Landlord[]>(this.LANDLORD_ENDPOINT)
-      .pipe(map(this.mapLandlordArrayToLandlordArrayBuild()))
-  }
-
-  getById(id: number): Observable<Landlord> {
-    if (!id) {
-      throw new Error('You must provide an id!')
-    }
-    return this.httpClient
-      .get<Landlord>(`${this.LANDLORD_ENDPOINT}/${id}`)
-      .pipe(map(this.mapLandlordToLandlordBuild()))
-  }
-
-  delete(id: number): Observable<Landlord> {
-    return this.httpClient.delete<Landlord>(`${this.LANDLORD_ENDPOINT}/${id}`)
-  }
-
-  add(landlord: Landlord): void {
-    this.httpClient.post(`${this.LANDLORD_ENDPOINT}`, landlord)
-  }
-
-  update(landlord: Landlord): Observable<Landlord> {
-    debugger
-    return this.httpClient.put<Landlord>(`${this.LANDLORD_ENDPOINT}`, landlord).pipe(
-      tap((_) => console.log(`updated landlord: ${_}`)),
-      map(this.mapLandlordToLandlordBuild()),
-      catchError(this.handleError)
-    )
-  }
-
-  toggleStatus(id: number): Observable<Landlord | null> {
-    // todo: implement toggle status logic
-    throw new Error('implement toggle status logic')
-    // const index = this.getArrayIndexById(id)
-    // if (typeof index === 'number') {
-    //   this.landlords[index].status = !this.landlords[index].status
-    //   return of(this.landlords[index])
-    // }
-    // return of(null)
-  }
+  constructor(private http: HttpClient) {}
 
   private mapLandlordArrayToLandlordArrayBuild() {
     return (landlords) => landlords.map(this.mapLandlordToLandlordBuild())
@@ -80,5 +28,47 @@ export class LandlordService {
 
       return of(result as T)
     }
+  }
+
+  getAll(): Observable<Landlord[]> {
+    return this.http
+      .get<Landlord[]>(this.landlordsUrl)
+      .pipe(map(this.mapLandlordArrayToLandlordArrayBuild()))
+  }
+
+  getById(id: number): Observable<Landlord> {
+    if (!id) {
+      throw new Error('You must provide an id!')
+    }
+    return this.http
+      .get<Landlord>(`${this.landlordsUrl}/${id}`)
+      .pipe(map(this.mapLandlordToLandlordBuild()))
+  }
+
+  add(landlord: any): Observable<Landlord> {
+    return this.http
+      .post<Landlord>(this.landlordsUrl, landlord)
+      .pipe(catchError(this.handleError<Landlord>('add')))
+  }
+
+  update(landlord: Landlord): Observable<Landlord | null> {
+    return this.http
+      .put<Landlord>(this.landlordsUrl, landlord)
+      .pipe(catchError(this.handleError<Landlord>('update')))
+  }
+
+  delete(id: number): Observable<Landlord> {
+    return this.http
+      .delete<Landlord>(`${this.landlordsUrl}/${id}`)
+      .pipe(catchError(this.handleError<Landlord>('delete')))
+  }
+
+  toggleStatus(landlord: Landlord): Observable<any> {
+    return this.http.patch<Landlord>(this.landlordsUrl, landlord).pipe(
+      map((landlord) => {
+        return (landlord.status = !landlord.status), console.log(landlord.status)
+      }),
+      catchError(this.handleError<Landlord>('update'))
+    )
   }
 }
