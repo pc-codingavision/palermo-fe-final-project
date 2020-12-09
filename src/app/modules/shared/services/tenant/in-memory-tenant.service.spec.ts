818203e18902a8453ea9f1451d48394bb92c89d7
Matteo Oliva <olivamatteus@gmail.com>
2020-12-09 23:20:30 +0100

    Added control flow and error handling

--- src/app/modules/shared/services/tenant/in-memory-tenant.service.spec.ts
+++ src/app/modules/shared/services/tenant/in-memory-tenant.service.spec.ts
@@ -1,8 +1,10 @@
+import { Overlay } from '@angular/cdk/overlay'
 import {
   HttpClientTestingModule,
   HttpTestingController,
 } from '@angular/common/http/testing'
 import { TestBed } from '@angular/core/testing'
+import { MatSnackBar } from '@angular/material/snack-bar'
 import { InMemoryTenantService } from '@modules/shared/services/tenant/in-memory-tenant.service'
 import { PhoneType, Role } from '@shared/enum/enums'
 import { TENANTS_MOCK_DATA } from '@shared/models/mock-data/data'
@@ -51,7 +53,7 @@ describe('InMemoryTenantService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [InMemoryTenantService],
+      providers: [InMemoryTenantService, MatSnackBar, Overlay],
     })
 
     httpTestingController = TestBed.inject(HttpTestingController)
@@ -78,221 +80,205 @@ describe('InMemoryTenantService', () => {
       req.flush(mockTenants)
       expect(data).toEqual(mockTenants)
     })
+  })
 
-    describe('GetById', () => {
-      it('should return tenant by passed id', () => {
-        let data
-        service.getById(1).subscribe((response) => {
-          data = response
-        })
-
-        const req = httpTestingController.expectOne('api/tenants/1')
-        expect(req.request.method).toEqual('GET')
-        req.flush(mockTenants[0])
-        expect(data).toEqual(mockTenants[0])
+  describe('GetById', () => {
+    it('should return tenant by passed id', () => {
+      let data
+      service.getById(1).subscribe((response) => {
+        data = response
       })
 
-      it('should throw errorResponse for not existing id ', () => {
-        let data
-        const errorResponse: ResponseOptions = {
-          body: { error: `'Tenants' with id=6 not found` },
-          status: 404,
-        }
-
-        service.getById(6).subscribe((response) => {
-          data = response
-        })
-
-        const req = httpTestingController.expectOne('api/tenants/6')
-        expect(req.request.method).toEqual('GET')
-        req.flush(errorResponse)
-        expect(data).toBe(errorResponse)
-      })
-
-      it('should throw an ErrorEvent for not existing id ', () => {
-        service.getById(6).subscribe(
-          (response) => {
-            throw new Error('Not Found')
-          },
-          (error) => {
-            expect(error.status).toEqual(404)
-            expect(error.statusText).toEqual(`Tenants' with id=6 not found`)
-          }
-        )
-
-        httpTestingController.expectOne('api/tenants/6').error(new ErrorEvent(''), {
-          status: 404,
-          statusText: `Tenants' with id=6 not found`,
-        })
-      })
+      const req = httpTestingController.expectOne('api/tenants/1')
+      expect(req.request.method).toEqual('GET')
+      req.flush(mockTenants[0])
+      expect(data).toEqual(mockTenants[0])
     })
 
-    describe('GetByName', () => {
-      it('should return all tenants who have passed Name', () => {
-        let data
-        service.getByName('ugo').subscribe((response) => {
-          data = response
-        })
+    it('should throw errorResponse for not existing id ', () => {
+      let data
+      const errorResponse: ResponseOptions = {
+        body: { error: `'Tenants' with id=6 not found` },
+        status: 404,
+      }
 
-        const req = httpTestingController.expectOne('api/tenants?name=ugo')
-        expect(req.request.method).toEqual('GET')
-        req.flush(mockTenants[0])
-        expect(data).toEqual(mockTenants[0])
+      service.getById(6).subscribe((response) => {
+        data = response
       })
 
-      it("should return an empty array if it doesn't find anything with passed name", () => {
-        let data
-        service.getByName('mario').subscribe((response) => {
-          data = response
-        })
-
-        const req = httpTestingController.expectOne('api/tenants?name=mario')
-        expect(req.request.method).toEqual('GET')
-        req.flush([])
-        expect(data.length).toBe(0)
-      })
-
-      it('should return an empty array if we pass null', () => {
-        let data
-        service.getByName(null).subscribe((response) => {
-          data = response
-        })
-
-        const req = httpTestingController.expectOne('api/tenants?name=null')
-        expect(req.request.method).toEqual('GET')
-        req.flush([])
-        expect(data.length).toBe(0)
-      })
+      const req = httpTestingController.expectOne('api/tenants/6')
+      expect(req.request.method).toEqual('GET')
+      req.flush(errorResponse)
+      expect(data).toBe(errorResponse)
     })
 
-    describe('GetBySurname', () => {
-      it('#getBySurname should return all tenants who have that Surname', () => {
-        let data
-        service.getBySurname('fantozzi').subscribe((response) => {
-          data = response
-        })
+    it('should throw an ErrorEvent for not existing id ', () => {
+      service.getById(6).subscribe()
 
-        const req = httpTestingController.expectOne('api/tenants?surname=fantozzi')
-        expect(req.request.method).toEqual('GET')
-        req.flush(mockTenants[0])
-        expect(data).toEqual(mockTenants[0])
-      })
-
-      it("#getBySurname should return an empty array if it doesn't find anything with that surname", () => {
-        let data
-        service.getBySurname('oliva').subscribe((response) => {
-          data = response
-        })
-
-        const req = httpTestingController.expectOne('api/tenants?surname=oliva')
-        expect(req.request.method).toEqual('GET')
-        req.flush([])
-        expect(data.length).toBe(0)
-      })
-
-      it('#getBySurname should return an empty array if we pass null', () => {
-        let data
-        service.getBySurname(null).subscribe((response) => {
-          data = response
-        })
-
-        const req = httpTestingController.expectOne('api/tenants?surname=null')
-        expect(req.request.method).toEqual('GET')
-        req.flush([])
-        expect(data.length).toBe(0)
+      httpTestingController.expectOne('api/tenants/6').error(new ErrorEvent(''), {
+        status: 404,
+        statusText: `Tenants' with id=6 not found`,
       })
     })
+  })
 
-    describe('GetByMail', () => {
-      it('should return all tenants who have passed email', () => {
-        let data
-        service.getByMail('rAg-fantOzzi@test.com').subscribe((response) => {
-          data = response
-        })
-
-        const req = httpTestingController.expectOne(
-          'api/tenants?mail=rAg-fantOzzi%40test.com'
-        )
-        expect(req.request.method).toEqual('GET')
-        req.flush(mockTenants[0])
-        expect(data).toEqual(mockTenants[0])
+  describe('GetByName', () => {
+    it('should return all tenants who have passed Name', () => {
+      let data
+      service.getByName('ugo').subscribe((response) => {
+        data = response
       })
+
+      const req = httpTestingController.expectOne('api/tenants?name=ugo')
+      expect(req.request.method).toEqual('GET')
+      req.flush(mockTenants[0])
+      expect(data).toEqual(mockTenants[0])
     })
 
-    describe('GetByStatus', () => {
-      it('should return all tenants who have status of true', () => {
-        let data
-        service.getByStatus(true).subscribe((response) => {
-          data = response
-        })
-
-        const req = httpTestingController.expectOne('api/tenants?status=true')
-        expect(req.request.method).toEqual('GET')
-        req.flush(mockTenants)
-        expect(data).toEqual(mockTenants)
+    it("should return null if it doesn't find anything with passed name", () => {
+      let data
+      service.getByName('mario').subscribe((response) => {
+        data = response
       })
 
-      it('should return all tenants who have that status of false', () => {
-        let data
-        service.getByStatus(false).subscribe((response) => {
-          data = response
-        })
-
-        const req = httpTestingController.expectOne('api/tenants?status=false')
-        expect(req.request.method).toEqual('GET')
-        req.flush([])
-        expect(data.length).toBe(0)
-      })
-
-      it('should return empty array if we pass null', () => {
-        let data
-        service.getByStatus(null).subscribe((response) => {
-          data = response
-        })
-
-        const req = httpTestingController.expectOne('api/tenants?status=null')
-        expect(req.request.method).toEqual('GET')
-        req.flush([])
-        expect(data.length).toBe(0)
-      })
+      const req = httpTestingController.expectOne('api/tenants?name=mario')
+      expect(req.request.method).toEqual('GET')
+      req.flush(null)
+      expect(data).toBeNull()
     })
 
-    describe('Add', () => {
-      it('should add a new tenant', () => {
-        service.add(newTenant).subscribe((response) => {
-          expect(response).toEqual(newTenant)
-        })
-
-        const req = httpTestingController.expectOne('api/tenants')
-        expect(req.request.method).toBe('POST')
-        req.flush(newTenant)
+    it('should return null if we pass null', () => {
+      let data
+      service.getByName(null).subscribe((response) => {
+        data = response
       })
+
+      httpTestingController.expectNone('api/tenants?name=null')
+      expect(data).toBeNull()
+    })
+  })
+
+  describe('GetBySurname', () => {
+    it('should return all tenants who have that Surname', () => {
+      let data
+      service.getBySurname('fantozzi').subscribe((response) => {
+        data = response
+      })
+
+      const req = httpTestingController.expectOne('api/tenants?surname=fantozzi')
+      expect(req.request.method).toEqual('GET')
+      req.flush(mockTenants[0])
+      expect(data).toEqual(mockTenants[0])
     })
 
-    describe('Update', () => {
-      it('#update should update an existing tenant with the same id', () => {
-        service.update(updatedTenant).subscribe((response) => {
-          expect(response.name.firstName).toBe('Silvio')
-          expect(response.id).toBe(1)
-        })
-        const req = httpTestingController.expectOne('api/tenants')
-        expect(req.request.method).toBe('PUT')
-        req.flush(updatedTenant)
+    it("should return an empty array if it doesn't find anything with that surname", () => {
+      let data
+      service.getBySurname('oliva').subscribe((response) => {
+        data = response
       })
+
+      const req = httpTestingController.expectOne('api/tenants?surname=oliva')
+      expect(req.request.method).toEqual('GET')
+      req.flush([])
+      expect(data.length).toBe(0)
     })
 
-    describe('Delete', () => {
-      it('should delete tenant with passed id', () => {
-        let data
-        service.delete(1).subscribe((response) => {
-          data = response
-        })
-
-        const req = httpTestingController.expectOne('api/tenants/1')
-        expect(req.request.method).toBe('DELETE')
-        req.flush(null)
-        expect(data).toBeNull()
+    it('should return null if we pass null', () => {
+      let data
+      service.getBySurname(null).subscribe((response) => {
+        data = response
       })
+
+      httpTestingController.expectNone('api/tenants?surname=null')
+
+      expect(data).toBeNull()
+    })
+  })
+
+  describe('GetByMail', () => {
+    it('should return all tenants who have passed email', () => {
+      let data
+      service.getByMail('rAg-fantOzzi@test.com').subscribe((response) => {
+        data = response
+      })
+
+      const req = httpTestingController.expectOne(
+        'api/tenants?mail=rAg-fantOzzi%40test.com'
+      )
+      expect(req.request.method).toEqual('GET')
+      req.flush(mockTenants[0])
+      expect(data).toEqual(mockTenants[0])
+    })
+
+    it('should return null if we pass null ', () => {
+      let data
+      service.getByMail(null).subscribe((response) => {
+        data = response
+      })
+      httpTestingController.expectNone('api/tenants?mail=rAg-fantOzzi%40test.com')
+      expect(data).toBeNull()
+    })
+  })
+
+  describe('GetByStatus', () => {
+    it('should return all tenants who have status of true', () => {
+      let data
+      service.getByStatus(true).subscribe((response) => {
+        data = response
+      })
+
+      const req = httpTestingController.expectOne('api/tenants?status=true')
+      expect(req.request.method).toEqual('GET')
+      req.flush(mockTenants)
+      expect(data).toEqual(mockTenants)
+    })
+
+    it('should return null if we pass null', () => {
+      let data
+      service.getByStatus(null).subscribe((response) => {
+        data = response
+      })
+
+      httpTestingController.expectNone('api/tenants?status=null')
+      expect(data).toBeNull()
+    })
+  })
+
+  describe('Add', () => {
+    it('should add a new tenant', () => {
+      service.add(newTenant).subscribe((response) => {
+        expect(response).toEqual(newTenant)
+      })
+
+      const req = httpTestingController.expectOne('api/tenants')
+      expect(req.request.method).toBe('POST')
+      req.flush(newTenant)
+    })
+  })
+
+  describe('Update', () => {
+    it('should update an existing tenant with the same id', () => {
+      service.update(updatedTenant).subscribe((response) => {
+        expect(response.name.firstName).toBe('Silvio')
+        expect(response.id).toBe(1)
+      })
+      const req = httpTestingController.expectOne('api/tenants')
+      expect(req.request.method).toBe('PUT')
+      req.flush(updatedTenant)
+    })
+  })
+
+  describe('Delete', () => {
+    it('should delete tenant with passed id', () => {
+      let data
+      service.delete(1).subscribe((response) => {
+        data = response
+      })
+
+      const req = httpTestingController.expectOne('api/tenants/1')
+      expect(req.request.method).toBe('DELETE')
+      req.flush(null)
+      expect(data).toBeNull()
     })
   })
 })
