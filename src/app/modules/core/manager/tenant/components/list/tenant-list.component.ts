912180edeb87df3f46a911efc8fdafa2a0fe0305
Stefano Rizzo <stefanorizzo295@gmail.com>
2020-12-10 23:17:11 +0100

    Added data from in-memory-tenant.service

--- src/app/modules/core/manager/tenant/components/list/tenant-list.component.ts
+++ src/app/modules/core/manager/tenant/components/list/tenant-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core'
+import { InMemoryTenantService } from '@modules/shared/services/tenant/in-memory-tenant.service'
+import { Tenant } from '@shared/models/tenant'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'cav-tenant-list',
@@ -6,7 +10,17 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./tenant-list.component.scss'],
 })
 export class TenantListComponent implements OnInit {
-  constructor() {}
+  displayedColumns: string[] = ['avatar', 'fullName', 'username', 'mail']
+  tenants$: Observable<Tenant[]>
+  constructor(private inMemoryTenantService: InMemoryTenantService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getAll()
+  }
+
+  getAll(): void {
+    this.tenants$ = this.inMemoryTenantService
+      .getAll()
+      .pipe(map((tenants) => tenants.map((tenant) => Tenant.Build(tenant))))
+  }
 }
