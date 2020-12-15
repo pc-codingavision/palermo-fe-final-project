ec3bf2f54d3b43506a0a8361238099aabb49621f
Carmelo <melonemark8@gmail.com>
2020-12-15 10:26:10 +0100

    Create search component

---
+++ src/app/modules/core/manager/tenant/components/search/tenant-search.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'cav-search',
+  templateUrl: './tenant-search.component.html',
+  styleUrls: ['./tenant-search.component.scss'],
+})
+export class TenantSearchComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
