eec9a22966fd17e42be62b5cbea6b03fd55b768c
Stefano Rizzo <stefanorizzo295@gmail.com>
2020-12-16 01:26:43 +0100

    implementation of expandable table rows

--- src/app/modules/core/manager/tenant/components/list/tenant-list.component.ts
+++ src/app/modules/core/manager/tenant/components/list/tenant-list.component.ts
@@ -1,15 +1,39 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { animate, state, style, transition, trigger } from '@angular/animations'
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
 import { Tenant } from '@shared/models/tenant'
+
+export interface Elements {
+  avatar: string
+  fullName: string
+  username: string
+  mail: string
+}
 
 @Component({
   selector: 'cav-tenant-list',
   templateUrl: './tenant-list.component.html',
   styleUrls: ['./tenant-list.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      ),
+    ]),
+  ],
 })
 export class TenantListComponent implements OnInit {
-  displayedColumns: string[] = ['avatar', 'fullName', 'username', 'mail']
   @Input() tenants: Tenant[]
+  @Output() update: EventEmitter<number | Tenant> = new EventEmitter<number | Tenant>()
+  displayedColumns: string[] = ['avatar', 'fullName', 'username', 'mail']
+  expandedElement: Elements | null
   constructor() {}
 
   ngOnInit(): void {}
+
+  updateTenant(event: number | Tenant): void {
+    this.update.emit(event)
+  }
 }
