29735d31c2a903a8be740e363583ea4d07dd6710
Stefano Rizzo <stefanorizzo295@gmail.com>
2020-12-16 01:28:11 +0100

    Logical implementation for changing the status and eliminating Tenant

--- src/app/modules/core/manager/tenant/components/container/tenant-container.component.ts
+++ src/app/modules/core/manager/tenant/components/container/tenant-container.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
 import { InMemoryTenantService } from '@modules/shared/services/tenant/in-memory-tenant.service'
 import { Tenant } from '@shared/models/tenant'
-import { Observable } from 'rxjs'
+import { Observable, Subscription } from 'rxjs'
 import { map } from 'rxjs/operators'
 
 @Component({
@@ -9,17 +9,31 @@ import { map } from 'rxjs/operators'
   templateUrl: './tenant-container.component.html',
   styleUrls: ['./tenant-container.component.scss'],
 })
-export class TenantContainerComponent implements OnInit {
+export class TenantContainerComponent implements OnInit, OnDestroy {
   tenants$: Observable<Tenant[]>
+  subscriptions: Subscription[] = []
   constructor(private inMemoryTenantService: InMemoryTenantService) {}
 
   ngOnInit(): void {
     this.getAll()
   }
 
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe())
+  }
+
   getAll(): void {
     this.tenants$ = this.inMemoryTenantService
       .getAll()
       .pipe(map((tenants) => tenants.map((tenant) => Tenant.Build(tenant))))
+  }
+
+  updateTenant(event: number | Tenant): void {
+    if (typeof event === 'number') {
+      this.subscriptions.push(this.inMemoryTenantService.delete(event).subscribe())
+      this.getAll()
+    } else {
+      this.subscriptions.push(this.inMemoryTenantService.update(event).subscribe())
+    }
   }
 }
