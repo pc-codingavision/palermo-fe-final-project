1f1ac70bb0c86e5c1721c1dc279e606708daf341
Piero Cascio <piero.cascio@gmail.com>
2020-12-16 11:07:51 +0100

    (fix): fix search not working properly.

--- src/app/modules/core/manager/tenant/components/container/tenant-container.component.ts
+++ src/app/modules/core/manager/tenant/components/container/tenant-container.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { MatTableDataSource } from '@angular/material/table'
 import { InMemoryTenantService } from '@modules/shared/services/tenant/in-memory-tenant.service'
 import { Tenant } from '@shared/models/tenant'
+import { SpinnerService } from '@shared/services/spinner.service'
 import { map } from 'rxjs/operators'
 
 @Component({
@@ -11,27 +12,72 @@ import { map } from 'rxjs/operators'
 })
 export class TenantContainerComponent implements OnInit {
   tenants: Tenant[]
+  filteredTenants: Tenant[]
   dataSource: MatTableDataSource<Tenant>
-  constructor(private inMemoryTenantService: InMemoryTenantService) {}
+  filterCriteria: { searchTerm: string; status: boolean }
 
-  ngOnInit(): void {
-    this.getAll()
+  constructor(
+    private inMemoryTenantService: InMemoryTenantService,
+    private spinnerService: SpinnerService
+  ) {
+    this.filterCriteria = {
+      searchTerm: '',
+      status: true,
+    }
   }
 
-  getAll(): void {
+  ngOnInit(): void {
+    this.spinnerService.showSpinner()
     this.inMemoryTenantService
       .getAll()
       .pipe(map((tenants) => tenants.map((tenant) => Tenant.Build(tenant))))
       .subscribe((tenants) => {
         this.dataSource = new MatTableDataSource<Tenant>(tenants)
+
+        this.dataSource.filterPredicate = this.customerFilter
+        this.dataSource.filter = JSON.stringify(this.filterCriteria)
+        this.spinnerService.hideSpinner()
       })
   }
 
   applyFilter(event: string): void {
-    this.dataSource.filter = event.trim().toLowerCase()
+    this.dataSource.filter = JSON.stringify(
+      Object.assign(this.filterCriteria, {
+        searchTerm: event.trim().toLowerCase(),
+      })
+    )
   }
 
-  selectStatus(event: string): void {
-    this.dataSource.filter = event
+  selectStatus(status: string): void {
+    console.log(`Status: ${status}`)
+    this.dataSource.filter = JSON.stringify(
+      Object.assign(this.filterCriteria, {
+        status: status === 'true',
+      })
+    )
+  }
+
+  private customerFilter: (tenant: Tenant, criteria: string) => boolean = (
+    tenant: Tenant,
+    criteria: string
+  ) => {
+    const filterCriteria: { searchTerm: string; status: boolean } = JSON.parse(criteria)
+
+    if (filterCriteria.searchTerm !== '') {
+      debugger
+      return (
+        (tenant.fullName
+          .toLowerCase()
+          .includes(filterCriteria.searchTerm.toLowerCase()) ||
+          tenant.username
+            .toLowerCase()
+            .includes(filterCriteria.searchTerm.toLowerCase()) ||
+          tenant.mail.toLowerCase().includes(filterCriteria.searchTerm.toLowerCase())) &&
+        tenant.status === filterCriteria.status
+      )
+    } else {
+      debugger
+      return tenant.status === filterCriteria.status
+    }
   }
 }
