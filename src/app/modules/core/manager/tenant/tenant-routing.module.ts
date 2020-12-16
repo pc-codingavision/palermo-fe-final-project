c6f09c0977e416f86e84772085e5f69ed1f58551
Stefano Rizzo <stefanorizzo295@gmail.com>
2020-12-16 17:58:37 +0100

    Delete path list and view in tenant-routing

--- src/app/modules/core/manager/tenant/tenant-routing.module.ts
+++ src/app/modules/core/manager/tenant/tenant-routing.module.ts
@@ -2,13 +2,9 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { TenantContainerComponent } from '@modules/core/manager/tenant/components/container/tenant-container.component'
 import { TenantEditComponent } from '@modules/core/manager/tenant/components/edit/tenant-edit.component'
-import { TenantListComponent } from '@modules/core/manager/tenant/components/list/tenant-list.component'
-import { TenantViewComponent } from '@modules/core/manager/tenant/components/view/tenant-view.component'
 
 const routes: Routes = [
   { path: '', component: TenantContainerComponent },
-  { path: 'list', component: TenantListComponent },
-  { path: 'view', component: TenantViewComponent },
   { path: 'edit', component: TenantEditComponent },
 ]
 
