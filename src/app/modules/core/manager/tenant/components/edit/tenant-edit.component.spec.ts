1aa3e5026cb628df20221bcfd371d7feb3a6d871
Stefano Rizzo <stefanorizzo295@gmail.com>
2020-12-19 01:21:26 +0100

    Added SnackBar, InMemoryTenantService and Overlay providers in tenant edit test

--- src/app/modules/core/manager/tenant/components/edit/tenant-edit.component.spec.ts
+++ src/app/modules/core/manager/tenant/components/edit/tenant-edit.component.spec.ts
@@ -1,5 +1,9 @@
+import { Overlay } from '@angular/cdk/overlay'
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
+import { MatSnackBar } from '@angular/material/snack-bar'
 import { TenantEditComponent } from '@modules/core/manager/tenant/components/edit/tenant-edit.component'
+import { InMemoryTenantService } from '@modules/shared/services/tenant/in-memory-tenant.service'
+import { commonTestingModules } from '@shared/common.testing'
 
 describe('TenantEditComponent', () => {
   let component: TenantEditComponent
@@ -9,6 +13,8 @@ describe('TenantEditComponent', () => {
     waitForAsync(() => {
       TestBed.configureTestingModule({
         declarations: [TenantEditComponent],
+        imports: [commonTestingModules],
+        providers: [InMemoryTenantService, MatSnackBar, Overlay],
       }).compileComponents()
     })
   )
